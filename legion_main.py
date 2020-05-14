05c4889532afe3d1fa3f746d9f859ef9bba9769f
Antoine Dujardin <dujardin@slac.stanford.edu>
2020-05-14 12:30:10 -0700

    Move hard-coded parameters out of legion_main.py.

--- legion_main.py
+++ legion_main.py
@@ -2,8 +2,9 @@ import h5py
 import numpy
 import os
 import pygion
-from pathlib import Path
 from pygion import acquire, attach_hdf5, task, Fspace, Ispace, Region, R
+
+import parms
 
 @task(privileges=[R])
 def print_region(data):
@@ -13,15 +14,11 @@ def print_region(data):
 def main():
     print("In main", flush=True)
 
-    det_shape = (4, 512, 512)
-    N_images = 1000
+    data = Region((parms.N_images,) + parms.det_shape,
+                  {'images': pygion.float64})
 
-    data_dir = Path(os.environ.get("DATA_DIR", ""))
-    data_path = data_dir / "2CEX-1.h5"
-
-    data = Region((N_images,) + det_shape, {'images': pygion.float64})
-
-    with attach_hdf5(data, str(data_path), {'images': 'slices'}, pygion.file_read_only):
+    with attach_hdf5(data, str(parms.data_path), {'images': 'slices'},
+                     pygion.file_read_only):
         pass
 
 if __name__ == '__main__':
