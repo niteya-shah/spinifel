610ec4e9cbaf62a14d2a4cf15d82b3dbdb717a5d
Antoine Dujardin <dujardin@slac.stanford.edu>
2020-06-01 15:42:38 -0700

    Sequential: move main to sequential.main.

--- sequential_main.py
+++ sequential_main.py
@@ -1,22 +1,4 @@
-from spinifel import parms
-from spinifel.sequential import get_data, solve_ac
-
-
-def main():
-    print("In sequential main", flush=True)
-
-    N_images = 1000
-    det_shape = parms.det_shape
-
-    (pixel_position_reciprocal,
-     pixel_distance_reciprocal,
-     pixel_index_map,
-     slices_) = get_data(N_images)
-
-    solve_ac(N_images,
-             pixel_position_reciprocal,
-             pixel_distance_reciprocal,
-             slices_)
+from spinifel.sequential import main
 
 
 if __name__ == '__main__':
--- spinifel/sequential/__init__.py
+++ spinifel/sequential/__init__.py
@@ -1,2 +1,3 @@
 from .prep import get_data
 from .autocorrelation import solve_ac
+from .main import main
