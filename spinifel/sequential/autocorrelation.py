99e6e713afb853697c55d62633f45e8568c4369a
Antoine Dujardin <dujardin@slac.stanford.edu>
2020-06-02 11:33:46 -0700

    Sequential: Add autocorrelation solver, fixes.

--- spinifel/sequential/autocorrelation.py
+++ spinifel/sequential/autocorrelation.py
@@ -1,14 +1,15 @@
+import finufftpy as nfft
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LogNorm
-from scipy.sparse.linalg import LinearOperator
+from scipy.sparse.linalg import LinearOperator, cg
 
 import pysingfel as ps
 
 from spinifel import parms, utils
 
 
-def forward(self, uvect, H, K, L, support, M, N,
+def forward(uvect, H, K, L, support, M, N,
             recip_extent, use_recip_sym):
     """Apply the forward, NUFFT2- problem."""
     if use_recip_sym:
@@ -23,7 +24,7 @@ def forward(self, uvect, H, K, L, support, M, N,
     return nuvect / M**3
 
 
-def adjoint(self, nuvect, H, K, L, support, M,
+def adjoint(nuvect, H, K, L, support, M,
             recip_extent, use_recip_sym):
     """Apply the adjoint, NUFFT1+ problem."""
     ugrid = np.zeros((M,)*3, dtype=np.complex, order='F')
@@ -38,27 +39,35 @@ def adjoint(self, nuvect, H, K, L, support, M,
     return uvect
 
 
-def solve_ac(N_images,
-             det_shape,
-             pixel_position_reciprocal,
+def solve_ac(pixel_position_reciprocal,
              pixel_distance_reciprocal,
              slices_):
+    Mquat = 10
+    M = 4 * Mquat + 1
+    Mtot = M**3
+    N_images = slices_.shape[0]
+    N = utils.prod(slices_.shape)
+    real_extent = 2
+    reciprocal_extent = pixel_distance_reciprocal.max()
+    use_reciprocal_symmetry = True
+
     orientations = ps.get_random_quat(N_images)
     rotmat = np.array([ps.quaternion2rot3d(quat) for quat in orientations])
     H, K, L = np.einsum("ijk,klmn->jilmn", rotmat, pixel_position_reciprocal)
     # shape -> [N_images] x det_shape
 
-    Mquat = 10
-    M = 4 * Mquat + 1
-    Mtot = M**3
-    N = N_images * utils.prod(det_shape)
-    real_extent = 2
-    reciprocal_extent = pixel_distance_reciprocal.max()
-    use_reciprocal_symmetry = True
+    data = slices_.flatten()
+    Hf = H.flatten()
+    Kf = K.flatten()
+    Lf = L.flatten()
 
     ac_support = np.ones((M,)*3)
     ac_estimate = np.zeros((M,)*3)
     weights = np.ones(N)
+
+    alambda = 1
+    rlambda = 1e-9
+    maxiter = 100
 
     idx = np.abs(L) < reciprocal_extent * .01
     plt.scatter(H[idx], K[idx], c=slices_[idx], s=1, norm=LogNorm())
@@ -96,3 +105,18 @@ def solve_ac(N_images,
     A._adjoint = lambda: A_adj
     A_adj._adjoint = lambda: A
     I._adjoint = lambda: I
+
+    def callback(xk):
+        callback.counter += 1
+        print('+', end='')
+    callback.counter = 0
+
+    x0 = ac_estimate.flatten()
+    b = data
+    al = alambda
+    rl = rlambda
+    W = al * A.H * D * A + rl * I
+    d = al * A.H * D * b + rl * x0
+    ret, info = cg(W, d, x0=x0, maxiter=maxiter, callback=callback)
+    print('')
+    return ret.reshape((M,)*3), callback.counter
