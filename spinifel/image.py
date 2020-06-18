e4350b41a2b211811165d06adfa7df9e24f2c4f3
Antoine Dujardin <dujardin@slac.stanford.edu>
2020-06-18 12:05:39 -0700

    Show x, y and z slices of volumes.

--- spinifel/image.py
+++ spinifel/image.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import cm
 from matplotlib.colors import LogNorm, SymLogNorm
 
 from spinifel import parms
@@ -17,11 +18,21 @@ def show_image(pixel_index_map, image, filename):
 
 
 def show_volume(ac, Mquat, filename):
-    ac_midz = ac[..., 2*Mquat]
     if ac.dtype == np.bool_:
-        ac_midz = ac_midz.astype(np.float)
-    plt.imshow(ac_midz, norm=SymLogNorm(1e-3))
-    plt.colorbar()
+        ac = ac.astype(np.float)
+    ac_midx = ac[2*Mquat, :, :]
+    ac_midy = ac[:, 2*Mquat, :]
+    ac_midz = ac[:, :, 2*Mquat]
+    vmin = 0
+    vmax = ac.max()
+
+    fig, axes = plt.subplots(figsize=(8.0, 2.5), nrows=1, ncols=3)
+    axes[0].imshow(ac_midx, norm=SymLogNorm(1e-3), vmin=vmin, vmax=vmax)
+    axes[1].imshow(ac_midy, norm=SymLogNorm(1e-3), vmin=vmin, vmax=vmax)
+    axes[2].imshow(ac_midz, norm=SymLogNorm(1e-3), vmin=vmin, vmax=vmax)
+
+    fig.colorbar(cm.ScalarMappable(norm=SymLogNorm(1e-3, vmin=vmin, vmax=vmax)),
+                 ax=axes.ravel().tolist(), fraction=0.04, pad=0.04)
+
     plt.savefig(parms.out_dir / filename)
-    plt.cla()
-    plt.clf()
+    plt.close('all')
