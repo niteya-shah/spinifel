1bd681b7000a08e4a929f90ad06e7427b169fca1
Antoine Dujardin <dujardin@slac.stanford.edu>
2020-07-29 15:26:10 -0700

    Add psana as flag for script.

--- scripts/run_cori.sh
+++ scripts/run_cori.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --account=m2859
 
-while getopts lmpsn:t: option
+while getopts lmpsan:t: option
 do
 case "${option}"
 in
@@ -14,6 +14,7 @@ l) USING_LEGION="1";;
 m) USING_MPI="1";;
 p) PROFILING="1";;
 s) SMALL_PROBLEM="1";;
+a) USE_PSANA="1";;
 n) NTASKS=$OPTARG;;
 t) OMP_NUM_THREADS=$OPTARG;;
 esac
@@ -49,7 +50,13 @@ if [[ -n $OMP_NUM_THREADS ]]; then
     echo "OMP_NUM_THREADS: $OMP_NUM_THREADS"
 fi
 
-export SMALL_PROBLEM
+if [[ -n $SMALL_PROBLEM ]]; then
+    export SMALL_PROBLEM
+fi
+
+if [[ -n $USE_PSANA ]]; then
+    export USE_PSANA
+fi
 
 if [[ $USING_LEGION -eq 1 ]]; then
     srun -n $NTASKS -N $nodes --cpus-per-task=$(( total_cores * 2 / (NTASKS / nodes) )) legion_python legion_main.py -ll:csize 16384 -ll:py 1 -ll:pyomp $(( total_cores - 2 ))
