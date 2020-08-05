4f8673ecaeafde019aabbe604644a59e552bfdac
Antoine Dujardin <dujardin@slac.stanford.edu>
2020-08-05 11:14:37 -0700

    Legion: Set PS_PARALLEL for psana import in pysingfel.

--- scripts/run_cori.sh
+++ scripts/run_cori.sh
@@ -59,6 +59,7 @@ if [[ -n $USE_PSANA ]]; then
 fi
 
 if [[ $USING_LEGION -eq 1 ]]; then
+    export PS_PARALLEL=legion
     srun -n $NTASKS -N $nodes --cpus-per-task=$(( total_cores * 2 / (NTASKS / nodes) )) legion_python legion_main.py -ll:csize 16384 -ll:py 1 -ll:pyomp $(( total_cores - 2 ))
 elif [[ $USING_MPI -eq 1 ]]; then
     export PS_PARALLEL=mpi
