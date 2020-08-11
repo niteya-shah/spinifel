305d310f2f8bcd03e27c68f1cfe71c34cca2fe5b
Antoine Dujardin <dujardin@slac.stanford.edu>
2020-08-10 17:23:15 -0700

    Legion: Use entire node memory.

--- scripts/run_cori.sh
+++ scripts/run_cori.sh
@@ -60,7 +60,7 @@ fi
 
 if [[ $USING_LEGION -eq 1 ]]; then
     export PS_PARALLEL=legion
-    srun -n $NTASKS -N $nodes --cpus-per-task=$(( total_cores * 2 / (NTASKS / nodes) )) legion_python legion_main.py -ll:csize 16384 -ll:py 1 -ll:pyomp $(( total_cores - 2 ))
+    srun -n $NTASKS -N $nodes --cpus-per-task=$(( total_cores * 2 / (NTASKS / nodes) )) legion_python legion_main.py -ll:csize 65536 -ll:py 1 -ll:pyomp $(( total_cores - 2 ))
 elif [[ $USING_MPI -eq 1 ]]; then
     export PS_PARALLEL=mpi
     srun -n $NTASKS python mpi_main.py
