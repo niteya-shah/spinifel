fae019b142145db4c68056a60cf89fdcc3e382ec
Johannes Blaschke <jpblaschke@lbl.gov>
2020-10-21 10:21:33 -0700

    output all settings when running in verbose mode

--- legion_main.py
+++ legion_main.py
@@ -1,5 +1,11 @@
 from spinifel.legion import main
+from spinifel        import SpinifelSettings
+
 
 
 if __name__ == '__main__':
+    settings = SpinifelSettings()
+    if settings.verbose:
+        print(settings)
+
     main()
--- mpi_main.py
+++ mpi_main.py
@@ -1,5 +1,11 @@
 from spinifel.mpi import main
+from spinifel     import SpinifelSettings
+
 
 
 if __name__ == '__main__':
+    settings = SpinifelSettings()
+    if settings.verbose:
+        print(settings)
+
     main()
