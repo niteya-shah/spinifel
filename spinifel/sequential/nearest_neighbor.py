9821ef49b486d868684adbb3cf7c34b35d367ee5
Johannes Blaschke <jpblaschke@lbl.gov>
2020-10-23 16:15:59 -0700

    fix oversight

--- spinifel/sequential/nearest_neighbor.py
+++ spinifel/sequential/nearest_neighbor.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
-from sklearn.metrics.pairwise import euclidean_distances
+from   sklearn.metrics.pairwise import euclidean_distances
 
 import spinifel.sequential.pyCudaKNearestNeighbors as pyCu
+from   spinifel import SpinifelSettings
 
 
 settings = SpinifelSettings()
@@ -11,22 +12,27 @@ settings = SpinifelSettings()
 
 def nearest_neighbor(model_slices, slices):
     if settings.using_cuda:
-        print("Using CUDA nearest neighbor.")
+        if settings.verbose:
+            print("Using CUDA nearest neighbor.")
+
         model_slices_flat = model_slices.flatten()
-        slices_flat = slices.flatten()
+        slices_flat       = slices.flatten()
+
         euDist = pyCu.cudaEuclideanDistance(slices_flat,
                                             model_slices_flat,
                                             slices.shape[0],
                                             model_slices.shape[0],
                                             slices.shape[1])
-        index = pyCu.cudaHeapSort(euDist,
-                                  slices.shape[0],
-                                  model_slices.shape[0],
-                                  slices.shape[1],
-                                  1)
+        index =  pyCu.cudaHeapSort(euDist,
+                                   slices.shape[0],
+                                   model_slices.shape[0],
+                                   slices.shape[1],
+                                   1)
     else:
-        print("Using sklearn nearest neighbor.")
-        ed = euclidean_distances(model_slices, slices)
+        if settings.verbose:
+            print("Using sklearn nearest neighbor.")
+
+        ed    = euclidean_distances(model_slices, slices)
         index = np.argmin(ed, axis=0)
 
     return index
