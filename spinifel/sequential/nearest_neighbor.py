ed85016ab070901fa5ddaa6d32909078f48d2311
Monarin Uervirojnangkoorn <monarin@gmail.com>
2020-10-30 20:19:12 -0400

    add multigpu seletion for orientation matching

--- spinifel/sequential/nearest_neighbor.py
+++ spinifel/sequential/nearest_neighbor.py
@@ -8,7 +8,8 @@ from   spinifel import SpinifelSettings
 
 settings = SpinifelSettings()
 
-
+from mpi4py import MPI
+rank = MPI.COMM_WORLD.Get_rank()
 
 def nearest_neighbor(model_slices, slices):
     if settings.using_cuda:
@@ -17,17 +18,22 @@ def nearest_neighbor(model_slices, slices):
 
         model_slices_flat = model_slices.flatten()
         slices_flat       = slices.flatten()
+        
+        deviceId = rank % settings._devices_per_node
+        print(f"Rank {rank} using deviceId {deviceId}")
 
         euDist = pyCu.cudaEuclideanDistance(slices_flat,
                                             model_slices_flat,
                                             slices.shape[0],
                                             model_slices.shape[0],
-                                            slices.shape[1])
+                                            slices.shape[1],
+                                            deviceId)
         index =  pyCu.cudaHeapSort(euDist,
                                    slices.shape[0],
                                    model_slices.shape[0],
                                    slices.shape[1],
-                                   1)
+                                   1,
+                                   deviceId)
     else:
         if settings.verbose:
             print("Using sklearn nearest neighbor.")
