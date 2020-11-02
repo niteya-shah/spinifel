c07aeafe1187570a004d4b5e31e128a474e2ae97
Monarin Uervirojnangkoorn <monarin@gmail.com>
2020-11-02 08:56:51 -0500

    Add no. of nodes/rs option

--- scripts/run_summit_mult.sh
+++ scripts/run_summit_mult.sh
@@ -7,16 +7,18 @@
 #BSUB -e error.%J.log         # error file name in which %J is replaced by the job ID
 #BSUB -o output.%J.log        # output file name in which %J is replaced by the job ID
 
-while getopts mscn:t:d: option
+while getopts mscn:t:d:g:N: option
 do
 case "${option}"
 in
 m) USING_MPI="1";;
 s) SMALL_PROBLEM="1";;
 c) USING_CUDA="1";;
-n) NTASKS=$OPTARG;;
+n) NTASKS_PER_NODE=$OPTARG;;
 t) OMP_NUM_THREADS=$OPTARG;;
 d) DATA_MULTIPLIER=$OPTARG;;
+g) DEVICES_PER_NODE=$OPTARG;;
+N) NNODES=$OPTARG;;
 esac
 done
 
@@ -37,16 +39,22 @@ export MPLCONFIGDIR=/gpfs/alpine/scratch/$USER/chm137/mtipProxy/writableDirector
 
 #export DATA_DIR=$SCRATCH/spinifel_data
 export DATA_DIR=/gpfs/alpine/proj-shared/chm137/data/spi
+#export DATA_DIR=/gpfs/alpine/proj-shared/chm137/iris
 
 #export OUT_DIR=$SCRATCH/spinifel_output
 export OUT_DIR=/gpfs/alpine/proj-shared/chm137/blaschke/spinifel_output
 mkdir -p $OUT_DIR
 rm -rf $OUT_DIR/*
 
-if [[ -z $NTASKS ]]; then
-        NTASKS="1"
+if [[ -z $NNODES ]]; then
+        NNODES="1"
 fi
-echo "NTASKS: $NTASKS"
+echo "NNODES: $NNODES"
+
+if [[ -z $NTASKS_PER_NODE ]]; then
+        NTASKS_PER_NODE="1"
+fi
+echo "NTASKS_PER_NODE: $NTASKS_PER_NODE"
 
 if [[ -n $OMP_NUM_THREADS ]]; then
     export OMP_NUM_THREADS
@@ -76,8 +84,14 @@ fi
 export DATA_MULTIPLIER
 echo "DATA_MULTIPLIER: $DATA_MULTIPLIER"
 
+if [[ -z $DEVICES_PER_NODE ]]; then
+    DEVICES_PER_NODE="6"
+fi
+export DEVICES_PER_NODE
+echo "DEVICES_PER_NODE: $DEVICES_PER_NODE"
+
 echo "MPI run"
 export PS_PARALLEL=mpi
 export VERBOSE=true
-export DEVICES_PER_NODE=6
-jsrun -n 1 -a 42 -c 42 -r 1 -g 6 python mpi_main.py
+#jsrun -n 1 -a 42 -c 42 -r 1 -g 6 python mpi_main.py
+jsrun -n $NNODES -a $NTASKS_PER_NODE -c $NTASKS_PER_NODE -g $DEVICES_PER_NODE -r 1 -l CPU-CPU -d packed -b packed:1 python mpi_main.py
