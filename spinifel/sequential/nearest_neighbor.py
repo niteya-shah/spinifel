4f4a2a212051f9ed6617867cce0152a322689ef0
Johannes Blaschke <jpblaschke@lbl.gov>
2020-11-02 16:37:47 -0800

    import guard for when pyCudaKNN not available

--- spinifel/sequential/nearest_neighbor.py
+++ spinifel/sequential/nearest_neighbor.py
@@ -2,11 +2,26 @@ import os
 import numpy as np
 from   sklearn.metrics.pairwise import euclidean_distances
 
-import spinifel.sequential.pyCudaKNearestNeighbors as pyCu
 from   spinifel import SpinifelSettings
 
 
 settings = SpinifelSettings()
+
+#_______________________________________________________________________________
+# TRY to import the cuda nearest neighbor pybind11 module -- if it exists in
+# the path and we enabled `using_cuda`
+
+from importlib.util import find_spec
+
+KNN_LOADER    = find_spec("spinifel.sequential.pyCudaKNearestNeighbors")
+KNN_AVAILABLE = KNN_LOADER is not None
+if settings.verbose:
+    print(f"pyCudaKNearestNeighbors is available: {KNN_AVAILABLE}")
+
+if settings.using_cuda and KNN_AVAILABLE:
+    import spinifel.sequential.pyCudaKNearestNeighbors as pyCu
+
+#-------------------------------------------------------------------------------
 
 
 
