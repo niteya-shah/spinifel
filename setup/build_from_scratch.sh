dc70ba206ca7054a90c3efd66c388f756b3033e3
Johannes Blaschke <jpblaschke@lbl.gov>
2020-12-17 13:48:22 -0500

    use submodules to manage dependency versions

--- setup/build_from_scratch.sh
+++ setup/build_from_scratch.sh
@@ -1,30 +1,54 @@
 #!/bin/bash
 
+
 set -e
+
 
 root_dir="$(dirname "${BASH_SOURCE[0]}")"
 pushd $root_dir
 
-# Clean up any previous installs.
-rm -rf conda
+
+#_______________________________________________________________________________
+# Set local install dir
+
+rm -rf install
+mkdir -p install/bin
+mkdir -p install/include
+mkdir -p install/lib
+mkdir -p install/share
+
+#-------------------------------------------------------------------------------
+
+
+#_______________________________________________________________________________
+# Set up environment
 
 # Make the env.sh and unenv.sh
 ./mk_env.sh
 
 source env.sh
+
 # sometimes LD_PRELOAD can inverfere with other scripts here -- temporarily
 # disable it (it is re-enabled at the end of this script)
 __LD_PRELOAD=$LD_PRELOAD
 unset LD_PRELOAD
 
-# Install Conda environment.
+#-------------------------------------------------------------------------------
+
+
+#_______________________________________________________________________________
+# Install conda and set up local environment
+
+# Clean up any previous installs.
+rm -rf conda
+
+# Get and run the miniconda installer
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$(uname -p).sh -O conda-installer.sh
 bash ./conda-installer.sh -b -p $CONDA_ROOT
 rm conda-installer.sh
 
 source $CONDA_ROOT/etc/profile.d/conda.sh
 
-# conda install -y conda-build # Must be installed in root environment
 PACKAGE_LIST=(
     python=$PYVER
     matplotlib
@@ -54,45 +78,79 @@ PACKAGE_LIST=(
 )
 
 conda create -y -p "$CONDA_ENV_DIR" "${PACKAGE_LIST[@]}" -c defaults -c anaconda
+
 conda activate "$CONDA_ENV_DIR"
+
 # Extra lcls2 deps
 conda install -y amityping -c lcls-ii
 conda install -y bitstruct krtc -c conda-forge
 
 CC=$MPI4PY_CC MPICC=$MPI4PY_MPICC pip install -v --no-binary mpi4py mpi4py
 
+#-------------------------------------------------------------------------------
+
+
+#_______________________________________________________________________________
+# Insall GASNET
+
 if [[ $USE_GASNET -eq 1 && $GASNET_ROOT == $PWD/gasnet/release ]]; then
-    rm -rf gasnet
-    git clone https://github.com/StanfordLegion/gasnet.git
     pushd gasnet
     make -j${THREADS:-8}
     popd
 fi
 
+#-------------------------------------------------------------------------------
+
+
+#_______________________________________________________________________________
+# Install Legion
+
 if [[ $LG_RT_DIR == $PWD/legion/runtime ]]; then
     rm -rf legion
-    rm -rf install
-    git clone -b control_replication https://gitlab.com/StanfordLegion/legion.git
     ./reconfigure_legion.sh
     ./rebuild_legion.sh
     cp "$CONDA_ENV_DIR"/lib/libhdf5* "$LEGION_INSTALL_DIR"/lib/
 fi
 
-rm -rf lcls2
+#-------------------------------------------------------------------------------
+
+
+#_______________________________________________________________________________
+# Install LCLS2 (aka PSANA2)
+
 git clone -b 3.2.0-legion https://github.com/slac-lcls/lcls2.git $LCLS2_DIR
 ./psana_clean_build.sh
+
+#-------------------------------------------------------------------------------
+
+
+#_______________________________________________________________________________
+# Rebuild FFTW (only needed on some systems -- that don't supply their own)
 
 if [[ $(hostname) = *"tulip"* || $(hostname) = *"jlse"* ]]; then
     ./rebuild_fftw.sh
 fi
 
-rm -rf finufft
-git clone https://github.com/elliottslaughter/finufft.git
+#-------------------------------------------------------------------------------
+
+
+#_______________________________________________________________________________
+# Install finufft
+
 ./rebuild_finufft.sh
 
-rm -rf pysingfel
-git clone https://github.com/AntoineDujardin/pysingfel.git
+#-------------------------------------------------------------------------------
+
+
+#_______________________________________________________________________________
+# Install pysingfel
+
 ./rebuild_pysingfel.sh
+
+#-------------------------------------------------------------------------------
+
+#_______________________________________________________________________________
+# Install cufinufft
 
 rm -rf cufinufft
 git clone -b spinifel https://github.com/JBlaschke/cufinufft.git
