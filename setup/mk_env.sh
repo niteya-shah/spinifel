15521966d92099e55c8264e4fd86f7bff8469e4e
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-01-11 17:05:56 -0800

    Try new approach for checking Cori host.

--- setup/mk_env.sh
+++ setup/mk_env.sh
@@ -51,7 +51,7 @@ _module_loaded () {
 EOF
 
 # Setup environment.
-if [[ $HOSTNAME = "cori"* ]]; then # On cori batch nodes, we don't have a usable $(hostname)
+if [[ $(hostname) = "cori"* || $SLURM_SUBMIT_HOST = "cori"* ]]; then
     cat >> env.sh <<EOF
 if _module_loaded PrgEnv-intel; then
     module swap PrgEnv-intel PrgEnv-gnu
