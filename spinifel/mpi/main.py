d4f513487bb4066ebb1181104852527eac9360f7
Monarin Uervirojnangkoorn <monarin@gmail.com>
2021-04-19 20:01:53 -0400

    forgot to recalculate model slice for new results

--- spinifel/mpi/main.py
+++ spinifel/mpi/main.py
@@ -76,6 +76,8 @@ def main():
     if DEBUG_FLAG:
         print(f'WARNING: Running in debug mode - known orientations and volumes will be used', flush=True)
         ivol = np.square(np.abs(volume))
+        rho_ = np.abs(np.fft.ifftn(ivol))
+        support_ = None
         ac_phased = np.fft.fftshift(np.abs(np.fft.ifftn(ivol))).astype(np.float32)
         ref_orientations = orientations_
 
@@ -101,20 +103,20 @@ def main():
     
     
     if DEBUG_FLAG:
-        print(f'Input data:')
-        print(f'  slices_: {slices_.shape} dtype: {slices_.dtype}')
-        print(f'  pixel_position_reciprocal: {pixel_position_reciprocal.shape} dtype: {pixel_position_reciprocal.dtype}')
-        print(f'  pixel_distance_reciprocal: {pixel_distance_reciprocal.shape} dtype: {pixel_distance_reciprocal.dtype}')
-        print(f'  reciprocal_extent: {reciprocal_extent} dtype: {type(reciprocal_extent)}')
+        print(f'Input data:', flush=True)
+        print(f'  slices_: {slices_.shape} dtype: {slices_.dtype}', flush=True)
+        print(f'  pixel_position_reciprocal: {pixel_position_reciprocal.shape} dtype: {pixel_position_reciprocal.dtype}', flush=True)
+        print(f'  pixel_distance_reciprocal: {pixel_distance_reciprocal.shape} dtype: {pixel_distance_reciprocal.dtype}', flush=True)
+        print(f'  reciprocal_extent: {reciprocal_extent} dtype: {type(reciprocal_extent)}', flush=True)
         
-        print(f'  ac_phased: {ac_phased.shape} dtype: {ac_phased.dtype}')
-        print(f'  model_slices: {model_slices.shape} dtype: {model_slices.dtype}')
-        print(f'  ref_orientations: {ref_orientations.shape} dtype: {ref_orientations.dtype}')
+        print(f'  ac_phased: {ac_phased.shape} dtype: {ac_phased.dtype}', flush=True)
+        print(f'  model_slices: {model_slices.shape} dtype: {model_slices.dtype}', flush=True)
+        print(f'  ref_orientations: {ref_orientations.shape} dtype: {ref_orientations.dtype}', flush=True)
 
 
     # scale model_slices
     data_model_scaling_ratio = slices_.std() / model_slices.std()
-    print(f"Data/Model std ratio: {data_model_scaling_ratio}.", flush=True)
+    logger.log(f"Data/Model std ratio: {data_model_scaling_ratio}.")
     model_slices *= data_model_scaling_ratio
     
     # Use imrovement of cc(prev_rho, cur_rho) to detemine if
@@ -123,22 +125,22 @@ def main():
     cov_delta = .05
     
     if DEBUG_FLAG: 
-        N_generations = 2 
+        N_generations = 3
     else:
         N_generations = parms.N_generations
 
     for generation in range(1, N_generations):
+        logger.log(f"Generation: {generation}")
         new_orientations = match(slices_, model_slices, 
                 ref_orientations, batch_size=parms.N_batch_size)
         logger.log(f"Orientations matched in {timer.lap():.2f}s.")
 
         if DEBUG_FLAG:
-            print("COMPARE KNOWN VERSUS COMPUTED ORIENTATIONS AND DOT PRODUCT")
+            print("COMPARE KNOWN VERSUS COMPUTED ORIENTATIONS AND DOT PRODUCT", flush=True)
             for i in range(slices_.shape[0]):
                 orientations_dot_prod = np.dot(ref_orientations[i], new_orientations[i])
-                print(ref_orientations[i], new_orientations[i],orientations_dot_prod)
+                print(i, ref_orientations[i], new_orientations[i],orientations_dot_prod, flush=True)
                 assert orientations_dot_prod - 1.0 < 1e12
-            exit()
 
         orientations = new_orientations
 
@@ -163,6 +165,16 @@ def main():
         logger.log(f"Problem phased in {timer.lap():.2f}s. cc={cov_xy:.2f} delta={cov_xy-prev_cov_xy:.2f}")
         if cov_xy - prev_cov_xy < cov_delta:
             break
+        
+        # Calulate new model slices from new phased ac using
+        # the original orientations.
+        model_slices = gen_model_slices_batch(ac_phased, ref_orientations, 
+                pixel_position_reciprocal, reciprocal_extent, parms.oversampling, 
+                ac_support_size, N_pixels, batch_size=parms.N_batch_size)
+        model_slices = model_slices.reshape((ref_orientations.shape[0], N_pixels))
+        logger.log(f"Slicing done in {timer.lap():.2f}s.")
+
+
         
 
     logger.log(f"Total: {timer.total():.2f}s.")
