8703dfe43c720107b1c6b44c1f40ab0f24da9ad5
Iris <iris@slac.stanford.edu>
2021-05-25 16:17:59 -0400

    make all ranks call sequential_phase() instead of broadcasting

--- spinifel/mpi/phasing.py
+++ spinifel/mpi/phasing.py
@@ -1,14 +1,11 @@
 from mpi4py import MPI
 
-import numpy  as np
-import PyNVTX as nvtx
+import numpy as np
 
 from spinifel import parms
 from spinifel.sequential.phasing import phase as sequential_phase
 
 
-
-@nvtx.annotate("mpi/phasing.py", is_prefix=True)
 def phase(generation, ac, support_=None, rho_=None):
     comm = MPI.COMM_WORLD
 
@@ -16,15 +13,7 @@ def phase(generation, ac, support_=None, rho_=None):
     M = 4*Mquat + 1
     Mtot = M**3
 
-    if comm.rank == 0:
-        ac_phased, support_, rho_ = sequential_phase(
-            generation, ac, support_, rho_)
-    else:
-        ac_phased = np.zeros((M,)*3, order="F", dtype=np.float32)
-        support_ = np.zeros((M,)*3, order="F", dtype=np.float32)
-        rho_ = np.zeros((M,)*3, order="F", dtype=np.float32)
-    comm.Bcast(ac_phased, root=0)
-    comm.Bcast(support_, root=0)
-    comm.Bcast(rho_, root=0)    
+    ac_phased, support_, rho_ = sequential_phase(
+        generation, ac, support_, rho_)
 
     return ac_phased, support_, rho_
