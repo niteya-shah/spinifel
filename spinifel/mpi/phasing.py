ecca0d5329b513d28b399f8dd8c46a02b8bd90f4
Iris <iris@slac.stanford.edu>
2021-05-25 14:39:13 -0400

    broadcast support_ and rho_ to all ranks

--- spinifel/mpi/phasing.py
+++ spinifel/mpi/phasing.py
@@ -24,5 +24,7 @@ def phase(generation, ac, support_=None, rho_=None):
         support_ = np.zeros((M,)*3, order="F", dtype=np.float32)
         rho_ = np.zeros((M,)*3, order="F", dtype=np.float32)
     comm.Bcast(ac_phased, root=0)
+    comm.Bcast(support_, root=0)
+    comm.Bcast(rho_, root=0)    
 
     return ac_phased, support_, rho_
