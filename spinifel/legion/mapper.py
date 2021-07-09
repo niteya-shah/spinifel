b269dacd716d9abce5da7387926202866dc376cf
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-07-09 16:41:19 -0700

    Switch to lifeline mapper in Python.

--- spinifel/legion/mapper.py
+++ spinifel/legion/mapper.py
@@ -23,12 +23,12 @@ import os
 import subprocess
 
 root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-simple_mapper_h_path = os.path.join(root_dir, 'legion_mapper', 'simple_mapper.h')
-simple_mapper_header = subprocess.check_output(['gcc', '-E', '-P', simple_mapper_h_path]).decode('utf-8')
+lifeline_mapper_h_path = os.path.join(root_dir, 'legion_mapper', 'lifeline_mapper.h')
+lifeline_mapper_header = subprocess.check_output(['gcc', '-E', '-P', lifeline_mapper_h_path]).decode('utf-8')
 legion_mappers_so_path = os.path.join(root_dir, 'legion_mapper', 'build', 'liblegion_mappers.so')
 
 ffi = pygion.ffi
-ffi.cdef(simple_mapper_header)
+ffi.cdef(lifeline_mapper_header)
 c = ffi.dlopen(legion_mappers_so_path)
 
-c.register_simple_mapper()
+c.register_lifeline_mapper()
