423075c587dde9e6277dc8e555719df1389bcdff
Iris <iris@slac.stanford.edu>
2021-07-29 21:39:26 -0400

    implement orientation matching in a distributed fashion

--- spinifel/legion/orientation_matching.py
+++ spinifel/legion/orientation_matching.py
@@ -15,6 +15,7 @@ from . import utils as lgutils
 def setup_match():
     print("setup_match")
     N_orientations = parms.N_orientations
+    N_images_per_rank = parms.N_images_per_rank
     N_batch_size = parms.N_batch_size
     fields_dict = {"quaternions": getattr(pygion, parms.data_type_str)}
     sec_shape = parms.quaternion_shape
@@ -25,8 +26,8 @@ def setup_match():
     print("N_orientations_per_rank =", N_orientations_per_rank)
     ref_orientations, ref_orientations_p = lgutils.create_distributed_region(
         N_orientations_per_rank, fields_dict, sec_shape)
-    match_summary = Region((N_procs * N_batch_size),
-                {"rank": pygion.int32, "index": pygion.int32, "euDist": pygion.float64})
+    match_summary = Region((N_procs * N_orientations_per_rank, N_images_per_rank),
+                {"euDist": pygion.float64})
     match_summary_p = Partition.equal(match_summary, (N_procs,))
     for i, ref_orientations_subr in enumerate(ref_orientations_p):
         prep.load_ref_orientations(ref_orientations_subr, i, N_orientations_per_rank, point=i)
@@ -40,27 +41,29 @@ def match_task(phased, slices, orientations, ref_orientations, match_summary, pi
     if parms.verbosity > 0:
         print(f"{socket.gethostname()} starts Orientation Matching.", flush=True)
         print(f"{socket.gethostname()}:", end=" ", flush=False)
-    index, euDist = sequential_match(
+    orientations_matched_local, minDist_local = sequential_match(
         phased.ac, slices.data, pixel_position.reciprocal, pixel_distance.reciprocal, ref_orientations.quaternions)
     if parms.verbosity > 0:
         print(f"{socket.gethostname()} finished Orientation Matching.", flush=True)
     match_summary.rank[0] = rank
-    match_summary.index[0] = index
-    match_summary.euDist[0] = euDist
+    match_summary.orientations_matched[0] = orientations_matched_local
+    match_summary.minDist[0] = minDist_local
 
 
-@task(privileges=[RO])
+@task(privileges=[RO, WD])
 @lgutils.gpu_task_wrapper
 @nvtx.annotate("legion/orientation_matching.py", is_prefix=True)
-def select_orientations(match_summary):
+def select_orientations(match_summary, orientations_selected):
     print('len(match_summary.rank) =', len(match_summary.rank))
     print('match_summary.rank =', match_summary.rank)
-    print('match_summary.index =', match_summary.index)
-    print('match_summary.euDist =', match_summary.euDist)
-    iref = np.argmin(match_summary.euDist)
-    ref_rank = match_summary.rank[iref]
-    print(f"Keeping result from rank {ref_rank}.", flush=True)
-    return iref
+    print('match_summary.orientations_matched =', match_summary.orientations_matched)
+    print('match_summary.minDist =', match_summary.minDist)
+    index = np.argmin(match_summary.minDist)
+    print('index =', index)
+    orientations_matched = np.swapaxes(match_summary.orientations_matched, 0, 1)
+    for i in range(len(index)):
+        orientations_selected.quaternions[:] = match_summary.orientations_matched[i,index,:]
+    return orientations_selected
 
 
 @nvtx.annotate("legion/orientation_matching.py", is_prefix=True)
@@ -70,16 +73,17 @@ def match(phased, slices, slices_p, pixel_position, pixel_distance, orientations
     # We can call the sequential function on each rank, provided that the
     # cost of generating the model_slices isn't prohibitive.
 
+    N_ranks_per_node =  parms.N_ranks_per_node ##### TO-DO
+    N_images_per_rank = parms.N_images_per_rank
     N_procs = Tunable.select(Tunable.GLOBAL_PYS).get()
     for i in IndexLaunch([N_procs]):
         # Ideally, the location (point) should be deduced from the
         # location of the slices.
         match_task(
-            phased, slices_p[i], orientations_p[i], ref_orientations_p[i], match_summary_p[i],
+            phased, slices_p[i], orientations_p[i/N_ranks_per_node], ref_orientations_p[i], match_summary_p[i],
             pixel_position, pixel_distance, i)
 
-    iref = select_orientations(match_summary)
-    print('iref =', iref)
-    seletced_index = match_summary.index[iref]
-    print('selected_index =', selected_index)
-    return orientations_p[selected_index.get()]
+    for i in IndexLaunch([N_procs/N_ranks_per_node]):
+        orientations_p[i] = select_orientations(match_summary, orientations_p[i])
+    
+    return orientations, orientations_p
