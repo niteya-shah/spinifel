e2cd801005eca58abc97713dc9e456427fe70adb
Iris <iris@slac.stanford.edu>
2021-08-05 19:31:25 -0400

    optimize sequential/orientation_matching.py

--- spinifel/sequential/orientation_matching.py
+++ spinifel/sequential/orientation_matching.py
@@ -38,9 +38,8 @@ def match(slices_, model_slices, ref_orientations, batch_size=None):
     return ref_orientations[index]
 
 
-
 @nvtx.annotate("sequential/orientation_matching.py", is_prefix=True)
-def slicing_and_match(ac, slices_, pixel_position_reciprocal, pixel_distance_reciprocal, ref_orientations):
+def slicing_and_match(ac, slices_, pixel_position_reciprocal, pixel_distance_reciprocal, ref_orientations, H_, K_, L_):
     """
     Determine orientations of the data images by minimizing the euclidean distance with the reference images 
     computed by randomly slicing through the autocorrelation.
@@ -63,8 +62,6 @@ def slicing_and_match(ac, slices_, pixel_position_reciprocal, pixel_distance_rec
 
     if not N_slices:
         return np.zeros((0, 4))
-
-    ref_rotmat = np.array([np.linalg.inv(skp.quaternion2rot3d(quat)) for quat in ref_orientations])
     
     reciprocal_extent = pixel_distance_reciprocal.max()
     pixel_position_rp_c = np.array(pixel_position_reciprocal, copy=False, order='C')
@@ -75,21 +72,31 @@ def slicing_and_match(ac, slices_, pixel_position_reciprocal, pixel_distance_rec
     N = N_pixels * N_orientations_per_rank
     slices_ = slices_.reshape((N_slices, N_pixels))
     model_slices_new = np.zeros((N,))
-    
+   
+    N_batch = N_pixels * N_batch_size
+
+    if H_ is None or K_ is None or L_ is None:
+        ref_rotmat = np.array([np.linalg.inv(skp.quaternion2rot3d(quat)) for quat in ref_orientations])
+
+        H_ = np.zeros((N_orientations_per_rank // N_batch_size, N_batch))
+        K_ = np.zeros((N_orientations_per_rank // N_batch_size, N_batch))
+        L_ = np.zeros((N_orientations_per_rank // N_batch_size, N_batch))
+
+        for i in range(N_orientations_per_rank // N_batch_size):
+            st = i * N_batch_size
+            en = st + N_batch_size
+            H, K, L = np.einsum("ijk,klmn->jilmn", ref_rotmat[st:en], pixel_position_rp_c)
+            H_[i,:] = H.flatten() / reciprocal_extent * np.pi / parms.oversampling
+            K_[i,:] = K.flatten() / reciprocal_extent * np.pi / parms.oversampling
+            L_[i,:] = L.flatten() / reciprocal_extent * np.pi / parms.oversampling
+
     st_slice = time.monotonic()
 
     for i in range(N_orientations_per_rank // N_batch_size):
-        st = i * N_batch_size
-        en = st + N_batch_size
-        H, K, L = np.einsum("ijk,klmn->jilmn", ref_rotmat[st:en], pixel_position_rp_c)
-        H_ = H.flatten() / reciprocal_extent * np.pi / parms.oversampling
-        K_ = K.flatten() / reciprocal_extent * np.pi / parms.oversampling
-        L_ = L.flatten() / reciprocal_extent * np.pi / parms.oversampling
-        N_batch = N_pixels * N_batch_size
         st_m = i * N_batch_size * N_pixels
         en_m = st_m + (N_batch_size * N_pixels)
         model_slices_new[st_m:en_m] = autocorrelation.forward(
-                ac, H_, K_, L_, 1, M, N_batch, reciprocal_extent, True).real
+                ac, H_[i], K_[i], L_[i], 1, M, N_batch, reciprocal_extent, True).real
         
     en_slice = time.monotonic()
     
@@ -105,4 +112,4 @@ def slicing_and_match(ac, slices_, pixel_position_reciprocal, pixel_distance_rec
     en_match = time.monotonic()
 
     print(f"Match tot:{en_match-st_init:.2f}s. slice={en_slice-st_slice:.2f}s. match={en_match-st_match:.2f}s. slice_oh={st_slice-st_init:.2f}s. match_oh={st_match-en_slice:.2f}s.")
-    return ref_orientations[index], minDist
+    return ref_orientations[index], minDist, H_, K_, L_
