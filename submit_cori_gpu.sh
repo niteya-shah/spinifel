13550044ac040357d38b6e35d3e739ff1167f975
Hsing-Yin Chang <iris@slac.stanford.edu>
2021-09-18 16:32:33 -0700

    add batch script for running on Cori GPU

---
+++ submit_cori_gpu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -A m1759
+#SBATCH -C gpu
+#SBATCH -q special
+#SBATCH -t 1:00:00
+#SBATCH -n 4
+#SBATCH --ntasks-per-node=4
+#SBATCH -c 10
+#SBATCH --gpus-per-task=1
+#SBATCH -J RunSpinifel
+#SBATCH -o RunSpinifel.%J
+#SBATCH -e RunSpinifel.%J
+
+t_start=`date +%s`
+
+# spinifel
+source setup/env.sh
+
+export SLURM_CPU_BIND="cores"
+srun python -m spinifel --default-settings=cgpu_quickstart.toml --mode=mpi
+
+t_end=`date +%s`
+echo PSJobCompleted TotalElapsed $((t_end-t_start)) $t_start $t_end
+
+
