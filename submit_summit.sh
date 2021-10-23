2d83c901a1ad396ca1118f7be9a4c5c5e0ec75ca
Iris <iris@slac.stanford.edu>
2021-10-23 14:11:42 -0400

    update sbatch script

--- submit_summit.sh
+++ submit_summit.sh
@@ -11,7 +11,8 @@ t_start=`date +%s`
 
 # spinifel
 source setup/env.sh
-export CUPY_CACHE_DIR=$PWD/setup/cupy
+export CUPY_CACHE_DIR=/tmp # used to be $PWD/setup/cupy
+export MPLCONFIGDIR=/tmp # writable directory for matplotlib
 
 jsrun -n 6 -a 1 -c 7 -g 1 -b rs -d packed python -m spinifel --default-settings=summit_mpi.toml --mode=mpi
 
