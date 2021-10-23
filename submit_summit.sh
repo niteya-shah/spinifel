37f7d05db9cc4ae6b30a2b4f6903d4c34182475c
Iris <iris@slac.stanford.edu>
2021-10-23 14:14:16 -0400

    update sbatch script

--- submit_summit.sh
+++ submit_summit.sh
@@ -14,7 +14,7 @@ source setup/env.sh
 export CUPY_CACHE_DIR=/tmp # used to be $PWD/setup/cupy
 export MPLCONFIGDIR=/tmp # writable directory for matplotlib
 
-jsrun -n 6 -a 1 -c 7 -g 1 -b rs -d packed python -m spinifel --default-settings=summit_mpi.toml --mode=mpi
+jsrun -n 6 -a 1 -c 7 -g 1 -b rs -d packed python -m spinifel --default-settings=summit_quickstart.toml --mode=mpi
 
 t_end=`date +%s`
 echo PSJobCompleted TotalElapsed $((t_end-t_start)) $t_start $t_end
