f5897f486fe4d72bfad8f827ee25a6b72073133b
Iris <iris@slac.stanford.edu>
2021-11-16 17:29:17 -0500

    make forward and adjoint both without M**3 in the denominator

--- spinifel/autocorrelation.py
+++ spinifel/autocorrelation.py
@@ -49,7 +49,7 @@ def forward(ugrid, H_, K_, L_, support, M, N, recip_extent, use_recip_sym):
     # Solve NUFFT2-
     nuvect = nufft_3d_t2(H_, K_, L_, ugrid, -1, 1e-12, N)
 
-    return nuvect / M**3
+    return nuvect #/ M**3
 
 
 
@@ -106,7 +106,7 @@ def core_problem_convolution(uvect, M, F_ugrid_conv_, M_ups, ac_support,
     ugrid_ups = xp.zeros((M_ups,) * 3, dtype=uvect.dtype)            
     ugrid_ups[:M, :M, :M] = ugrid
     # Convolution = Fourier multiplication
-    F_ugrid_ups = xp.fft.fftn(xp.fft.ifftshift(ugrid_ups)) / M**3
+    F_ugrid_ups = xp.fft.fftn(xp.fft.ifftshift(ugrid_ups)) #/ M**3
     F_ugrid_conv_out_ups = F_ugrid_ups * F_ugrid_conv_
     ugrid_conv_out_ups = xp.fft.fftshift(xp.fft.ifftn(F_ugrid_conv_out_ups))
     # Downsample
@@ -136,7 +136,7 @@ def fourier_reg(uvect, support, F_antisupport, M, use_recip_sym):
         support = xp.asarray(support)
         F_antisupport = xp.asarray(F_antisupport)
 
-    F_ugrid = xp.fft.fftn(xp.fft.ifftshift(ugrid)) / M**3
+    F_ugrid = xp.fft.fftn(xp.fft.ifftshift(ugrid)) #/ M**3
     F_reg = F_ugrid * xp.fft.ifftshift(F_antisupport)
     reg = xp.fft.fftshift(xp.fft.ifftn(F_reg))
     uvect = (reg * support).flatten()
