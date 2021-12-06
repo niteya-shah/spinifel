f74eae5c80bdb88844cb3e5b50a5d8be4ff1a1d4
Hsing-Yin Chang <iris@slac.stanford.edu>
2021-12-06 09:05:29 -0800

    make sure every rank returns the same ac_phased

--- spinifel/mpi/phasing.py
+++ spinifel/mpi/phasing.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from mpi4py import MPI
+
 from spinifel import settings, contexts
 from spinifel.sequential.phasing import phase as sequential_phase
 
@@ -7,6 +9,7 @@ from spinifel.sequential.phasing import phase as sequential_phase
 def phase(generation, ac, support_=None, rho_=None):
     """Phase retrieval by Rank0 and broadcast to all ranks."""
 
+    comm = MPI.COMM_WORLD
     Mquat = settings.Mquat
     M = 4*Mquat + 1
     Mtot = M**3
@@ -14,4 +17,9 @@ def phase(generation, ac, support_=None, rho_=None):
     ac_phased, support_, rho_ = sequential_phase(
         generation, ac, support_, rho_)
 
+    comm.Bcast(ac_phased, root=0)
+    comm.Bcast(support_, root=0)
+    comm.Bcast(rho_, root=0)
+
     return ac_phased, support_, rho_
+
