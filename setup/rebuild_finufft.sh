eedcd71ff8b6615d2d169afc914beda9efe37d86
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-02-02 14:25:30 -0500

    Switch to out-of-tree finufft configuration and disable single-precision build.

--- setup/rebuild_finufft.sh
+++ setup/rebuild_finufft.sh
@@ -5,12 +5,31 @@ set -e
 root_dir="$(dirname "${BASH_SOURCE[0]}")"
 source "$root_dir"/env.sh
 
+# Enable host overwrite
+target=${SPINIFEL_TARGET:-${NERSC_HOST:-$(hostname --fqdn)}}
+
 pushd "$root_dir"/finufft
 
+if [[ ${target} = *"summit"* || ${target} = *"ascent"* ]]; then
+    MARCH_FLAGS="-mcpu=native"
+else
+    MARCH_FLAGS="-march=native"
+fi
+
+if [[ $CXX = "clang"* ]]; then
+    OPT_FLAGS=""
+else
+    OPT_FLAGS="-fcx-limited-range"
+fi
+
 cat > make.inc <<EOF
-CFLAGS+=$FINUFFT_CFLAGS
-CXXFLAGS+=$FINUFFT_CFLAGS
+CC:=$CC
+CXX:=$CXX
+CFLAGS:=-fPIC -O3 -funroll-loops $MARCH_FLAGS $OPT_FLAGS $FINUFFT_CFLAGS
+FFLAGS:=\$(CFLAGS)
+CXXFLAGS:=\$(CFLAGS) -DNEED_EXTERN_C
 LIBS+=$FINUFFT_LDFLAGS
+ENABLE_SINGLE:=OFF
 EOF
 
 make -j${THREADS:-8} lib
