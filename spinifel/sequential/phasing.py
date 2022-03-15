63ef1ef15a2f13d074cc812261a866371a202526
Hsing-Yin Chang <iris@slac.stanford.edu>
2022-03-14 17:12:38 -0700

    add Fourier HIO implementation in phasing

--- spinifel/sequential/phasing.py
+++ spinifel/sequential/phasing.py
@@ -7,6 +7,7 @@ from spinifel import SpinifelSettings, settings, image
 # settings = SpinifelSettings()
 
 xp = np
+#print('settings.use_cupy =', settings.use_cupy)
 if settings.use_cupy:
     if settings.verbose:
         print(f"Using CuPy for FFTs.")
@@ -91,10 +92,10 @@ def create_support_(ac_, M, Mquat, generation):
     square_support = xp.zeros((M, M, M), dtype=xp.bool_)
     square_support[sl, sl, sl] = 1
     square_support_ = xp.fft.ifftshift(square_support)
-    image.show_volume(square_support, Mquat, f"square_support_{generation}.png")
+    #image.show_volume(square_support, Mquat, f"square_support_{generation}.png")
 
     thresh_support_ = ac_ > 1e-2 * ac_.max()
-    image.show_volume(np.fft.fftshift(thresh_support_), Mquat, f"thresh_support_{generation}.png")
+    #image.show_volume(np.fft.fftshift(thresh_support_), Mquat, f"thresh_support_{generation}.png")
 
     return xp.logical_and(square_support_, thresh_support_)
 
@@ -177,12 +178,14 @@ def step_phase(rho_, amplitudes_, amp_mask_, support_):
     """
     rho_hat_ = xp.fft.fftn(rho_)
     phases_ = xp.angle(rho_hat_)
-    rho_hat_mod_ = xp.where(
-        amp_mask_,
-        amplitudes_ * xp.exp(1j*phases_),
-        rho_hat_)
+    # changes below
+    rho_filter_ = xp.where(support_, rho_, 0) # added
+    rho_hat_filter_ = xp.fft.fftn(rho_filter_) # added
+    #rho_hat_mod_ = xp.where(amp_mask_,amplitudes_ * xp.exp(1j*phases_),rho_hat_)
+    rho_hat_mod_ = xp.where(amp_mask_, amplitudes_*xp.exp(1j*phases_), rho_hat_filter_) # added
     rho_mod_ = xp.fft.ifftn(rho_hat_mod_).real
-    support_star_ = xp.logical_and(support_, rho_mod_>0)
+    #support_star_ = xp.logical_and(support_, rho_mod_>0) # might need to remove it # charge flipping?
+    support_star_ = support_ # changed
     return rho_mod_, support_star_
 
 
@@ -197,7 +200,6 @@ def shrink_wrap(cutoff, sigma, rho_, support_):
     :param rho_: electron density estimate
     :param support_: object support
     """
-    print('cutoff =', cutoff)
     #rho_abs_ = xp.absolute(rho_)
     avg = np.mean(rho_)
     sign = np.sign(avg)
@@ -207,9 +209,9 @@ def shrink_wrap(cutoff, sigma, rho_, support_):
         rho_abs_ = -rho_
     # By using 'wrap', we don't need to fftshift it back and forth
     rho_gauss_ = gaussian_filter(
-        rho_abs_, mode='wrap', sigma=sigma, truncate=2)
-    print('rho_abs_.max() =', rho_abs_.max())
-    support_[:] = rho_gauss_ > rho_abs_.max() * cutoff
+        rho_abs_, mode='wrap', sigma=sigma, truncate=1) # truncate=5 changed
+    #support_[:] = rho_gauss_ > rho_abs_.max() * cutoff
+    support_[:] = rho_gauss_ > rho_gauss_.max() * cutoff # changed
 
 
 
@@ -238,51 +240,65 @@ def phase(generation, ac, support_=None, rho_=None):
     Mtot = M**3
 
     ac = xp.array(ac)
-    ac_filt = gaussian_filter(xp.maximum(ac.real, 0), mode='constant',
-                              sigma=1, truncate=2)
-    image.show_volume(ac_filt, Mquat, f"autocorrelation_filtered_{generation}.png")
+    #ac_filt = gaussian_filter(xp.maximum(ac.real, 0), mode='constant',
+    #                          sigma=1, truncate=5) # changed
+    ac_filt = ac.real
+    print('type(ac_filt) =', type(ac_filt))
+    #image.show_volume(ac_filt, Mquat, f"autocorrelation_filtered_{generation}.png")
     ac_filt_ = xp.fft.ifftshift(ac_filt)
 
     intensities_ = xp.abs(xp.fft.fftn(ac_filt_))
-    image.show_volume(xp.fft.fftshift(intensities_), Mquat, f"intensities_{generation}.png")
+    ## set intensity corners to 0 # added
+    #lu = np.linspace(-np.pi, np.pi, M)
+    #Hu_, Ku_, Lu_ = np.meshgrid(lu, lu, lu, indexing='ij')
+    #Qu_ = np.sqrt(Hu_**2+Ku_**2+Lu_**2)
+    #print('Qu_.shape', Qu_.shape)
+    #q_corners = np.where(Qu_[:,:,:]>np.pi/settings.oversampling)
+    #intensities = xp.fft.fftshift(intensities_)
+    #intensities[q_corners] = 0
+    #intensities_ = xp.fft.ifftshift(intensities)
+    #image.show_volume(xp.fft.fftshift(intensities_), Mquat, f"intensities_{generation}.png")
 
     amplitudes_ = xp.sqrt(intensities_)
-    image.show_volume(xp.fft.fftshift(amplitudes_), Mquat, f"amplitudes_{generation}.png")
+    #image.show_volume(xp.fft.fftshift(amplitudes_), Mquat, f"amplitudes_{generation}.png")
 
     amp_mask_ = xp.ones((M, M, M), dtype=xp.bool_)
     # Mask out central peak
     amp_mask_[0, 0, 0] = 0
     amp_mask = xp.fft.fftshift(amp_mask_)
     # Mask out corners
-    lc = np.linspace(-M/2+1, M/2, M)
-    Hc_, Kc_, Lc_ = np.meshgrid(lc, lc, lc, indexing='ij')
-    Qc_ = np.sqrt(Hc_**2+Kc_**2+Lc_**2)
-    corners = np.where(Qc_[:,:,:]>(M+1)/2)
+    lc = xp.linspace(-M/2+1, M/2, M)
+    Hc_, Kc_, Lc_ = xp.meshgrid(lc, lc, lc, indexing='ij')
+    Qc_ = xp.sqrt(Hc_**2+Kc_**2+Lc_**2)
+    corners = xp.where(Qc_[:,:,:]>(M+1)/2)
     amp_mask[corners] = 0
-    image.show_volume(amp_mask, Mquat, f"amp_mask_{generation}.png")
-    #image.show_volume(xp.fft.fftshift(amp_mask_), Mquat, f"amp_mask_{generation}.png")
+    #image.show_volume(amp_mask, Mquat, f"amp_mask_{generation}.png")
+
+    amp_mask_ = xp.fft.ifftshift(amp_mask)
 
     if support_ is None:
         support_ = create_support_(ac_filt_, M, Mquat, generation)
-    image.show_volume(xp.fft.fftshift(support_), Mquat, f"support_{generation}.png")
+    #image.show_volume(xp.fft.fftshift(support_), Mquat, f"support_{generation}.png")
     support_ = xp.array(support_)
 
     if rho_ is None:
         rho_ = np.zeros((M, M, M)) # changed
         #rho_ = support_ * xp.random.rand(*support_.shape)
-    image.show_volume(xp.fft.fftshift(rho_), Mquat, f"rho_{generation}.png")
+    #image.show_volume(xp.fft.fftshift(rho_), Mquat, f"rho_{generation}.png")
     rho_ = xp.array(rho_)
     
     rho_max = xp.infty
 
     nER = settings.nER
     nHIO = settings.nHIO
+    beta = settings.beta
+    cutoff = settings.cutoff
 
     for i in range(settings.N_phase_loops):
         ER_loop(nER, rho_, amplitudes_, amp_mask_, support_, rho_max)
-        HIO_loop(nHIO, settings.beta, rho_, amplitudes_, amp_mask_, support_, rho_max)
+        HIO_loop(nHIO, beta, rho_, amplitudes_, amp_mask_, support_, rho_max)
         ER_loop(nER, rho_, amplitudes_, amp_mask_, support_, rho_max)
-        shrink_wrap(settings.cutoff, 1, rho_, support_)
+        shrink_wrap(cutoff, 1, rho_, support_)
     ER_loop(nER, rho_, amplitudes_, amp_mask_, support_, rho_max)
 
     if settings.use_cupy:
@@ -295,20 +311,18 @@ def phase(generation, ac, support_=None, rho_=None):
 
     recenter(rho_, support_, M)
 
-    image.show_volume(np.fft.fftshift(rho_), Mquat, f"rho_phased_{generation}.png")
+    #image.show_volume(np.fft.fftshift(rho_), Mquat, f"rho_phased_{generation}.png")
 
     intensities_phased_ = np.abs(np.fft.fftn(rho_))**2
-    image.show_volume(np.fft.fftshift(intensities_phased_), Mquat, f"intensities_phased_{generation}.png")
+    #image.show_volume(np.fft.fftshift(intensities_phased_), Mquat, f"intensities_phased_{generation}.png")
 
     #ac_phased_ = np.abs(np.fft.ifftn(intensities_phased_))
     ac_phased_ = np.fft.ifftn(intensities_phased_) # changed
     ac_phased = np.fft.fftshift(ac_phased_)
     factor = np.linalg.norm(ac_phased.real)/np.linalg.norm(ac_phased.imag)
     print("factor =", factor)
-    print("ac_phased =", ac_phased)
     ac_phased = ac_phased.real
-    image.show_volume(ac_phased, Mquat, f"autocorrelation_phased_{generation}.png")
+    #image.show_volume(ac_phased, Mquat, f"autocorrelation_phased_{generation}.png")
 
     ac_phased = ac_phased.astype(np.float32)
-
     return ac_phased, support_, rho_
