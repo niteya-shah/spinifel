81174b2970ff222cfe2dc95ea66f1e227f758f54
monarin <monarin@gmail.com>
2022-06-16 15:23:19 -0700

    clone testdata if not given

---
+++ tests/run_cgpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# Clone test data and save them to test_data_dir
+if [ -z ${test_data_dir+x} ]; then
+    echo "[spinifel/test] - test_data_dir is not set. The testdata will be cloned"
+    test_data_root="/tmp" 
+    export test_data_dir="${test_data_root}/spinifel_testdata_$$"
+    echo "[spinifel/test] - Cloning testdata to $test_data_dir"
+    git clone https://gitlab.osti.gov/mtip/data/testdata.git $test_data_dir
+fi
+
+
+export SPINIFEL_TEST_MODULE="ORIENTATION_MATCHING"
+srun -n 1 python -u -m spinifel --settings=./settings/test_mpi.toml --mode=test
+
+
+export SPINIFEL_TEST_MODULE="MAIN_PSANA2"
+srun -n 3 python -u -m spinifel --settings=./settings/test_mpi.toml --mode=test
