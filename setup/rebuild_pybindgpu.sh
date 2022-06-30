c4314167d001e06801f88895f3582e33a065ce93
darrenjhsu <hsudj@ornl.gov>
2022-06-30 16:39:57 -0400

    Include pybindgpu.sh

---
+++ setup/rebuild_pybindgpu.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+root_dir="$(dirname "${BASH_SOURCE[0]}")"
+source "$root_dir"/env.sh
+
+# Enable host overwrite
+target=${SPINIFEL_TARGET:-${NERSC_HOST:-$(hostname --fqdn)}}
+echo $target
+pushd "$root_dir"/PybindGPU/PyGPU
+
+if [[ ${target} = *"crusher"* ]]; then
+    make 
+elif [[ ${target} = *"spock"* ]]; then
+    echo "Currently unsupported"
+    exit
+else
+    echo "We do not need PybindGPU for this architecture"
+    exit
+fi
+popd
+pushd "$root_dir"/PybindGPU
+if [[ ${target} = *"crusher"* ]]; then
+    pip install --no-cache-dir -e .
+fi
+popd
