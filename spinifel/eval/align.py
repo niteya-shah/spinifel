b1cab0160ca38e5c5ed4b592cb41012396d19e11
niteya <niteya@lanl.gov>
2022-07-22 10:28:16 -0600

    Improved FSC calculation changes

--- spinifel/eval/align.py
+++ spinifel/eval/align.py
@@ -1,20 +1,12 @@
 import numpy as np
 import mrcfile
 from geom import *
-from scipy import ndimage
 import skopi as sk
-if use_cupy:
-    import cupy as xp
-    from cupyx.scipy import ndimage
-else:
-    from scipy import ndimage
-    xp = np
-
 
 def save_mrc(output, data, voxel_size=None, header_origin=None):
     """
     Save numpy array as an MRC file.
-    
+
     Parameters
     ----------
     output : string, default None
@@ -40,17 +32,17 @@ def save_mrc(output, data, voxel_size=None, header_origin=None):
     mrc.close()
     return
 
-def rotate_volume(vol, quat):
+def rotate_volume(vol, quat, xp, ndimage):
     """
     Rotate copies of the volume by the given quaternions.
-    
+
     Parameters
     ----------
     vol : numpy.ndarray, shape (n,n,n)
         volume to be rotated
     quat : numpy.ndarray, shape (n_quat,4)
         quaternions to apply to the volume
-        
+
     Returns
     -------
     rot_vol : numpy.ndarray, shape (n_quat,n,n,n)
@@ -58,71 +50,71 @@ def rotate_volume(vol, quat):
     """
     M = vol.shape[0]
     lincoords = xp.arange(M)
-    coords = xp.meshgrid(lincoords,lincoords,lincoords)
+    coords = xp.meshgrid(lincoords, lincoords, lincoords)
 
-    xyz = xp.vstack([coords[0].reshape(-1) - int(M/2),
-                     coords[1].reshape(-1) - int(M/2),
-                     coords[2].reshape(-1) - int(M/2)])
+    xyz = xp.vstack([coords[0].reshape(-1) - int(M / 2),
+                     coords[1].reshape(-1) - int(M / 2),
+                     coords[2].reshape(-1) - int(M / 2)])
 
-    R = quaternion2rot3d(quat)
-    transformed_xyz = xp.dot(R,xyz) + int(M/2)
-    
-    new_xyz = xp.array([transformed_xyz[:,1,:].flatten(), 
-               transformed_xyz[:,0,:].flatten(),
-               transformed_xyz[:,2,:].flatten()]) 
+    R = quaternion2rot3d(quat, xp)
+    transformed_xyz = xp.dot(R, xyz) + int(M / 2)
+
+    new_xyz = xp.array([transformed_xyz[:, 1, :].flatten(),
+                        transformed_xyz[:, 0, :].flatten(),
+                        transformed_xyz[:, 2, :].flatten()])
     rot_vol = ndimage.map_coordinates(vol, new_xyz, order=1)
-    rot_vol = rot_vol.reshape((quat.shape[0],M,M,M))
+    rot_vol = rot_vol.reshape((quat.shape[0], M, M, M))
     return rot_vol
 
-def center_volume(vol):
+def center_volume(vol, xp, ndimage):
     """
     Apply translational shifts to center the density within the volume.
-      
+
     Parameters
     ----------
     vol : numpy.ndarray, shape (n,n,n)
         volume to be centered
-        
+
     Returns
     -------
     cen_vol : numpy.ndarray, shape (n,n,n)
         centered volume
     """
     old_center = xp.array(ndimage.measurements.center_of_mass(vol))
-    new_center = xp.array(xp.array(vol.shape)/2).astype(int)
-    cen_vol = ndimage.shift(vol, -1*(old_center - new_center))
+    new_center = xp.array(xp.array(vol.shape) / 2).astype(int)
+    cen_vol = ndimage.shift(vol, -1 * (old_center - new_center))
     return cen_vol
-    
-def pearson_cc(arr1, arr2):
+
+def pearson_cc(arr1, arr2, xp):
     """
     Compute the Pearson correlation-coefficient between the input arrays.
-    
+
     Parameters
     ----------
     arr1 : numpy.ndarray, shape (n_samples, n_points)
         input array
     arr2 : numpy.ndarray, shape (n_samples, n_points) or (1, n_points)
         input array to compute CC with
-    
+
     Returns
     -------
     ccs : numpy.ndarray, shape (n_samples)
         correlation coefficient between paired sample arrays, or if
         arr2.shape[0] == 1, then between each sample of arr1 to arr2
     """
-    vx = arr1 - arr1.mean(axis=-1)[:,None]
-    vy = arr2 - arr2.mean(axis=-1)[:,None]
+    vx = arr1 - arr1.mean(axis=-1)[:, None]
+    vy = arr2 - arr2.mean(axis=-1)[:, None]
     numerator = xp.sum(vx * vy, axis=1)
     # Merge Square root because we have squares
     denom = xp.sqrt(xp.sum(vx**2, axis=1)) * xp.sqrt(xp.sum(vy**2, axis=1))
     return numerator / denom
 
-def score_deformations(mrc1, mrc2, warp):
+def score_deformations(mrc1, mrc2, warp, xp, ndimage):
     """
-    Compute the Pearson correlation coefficient between the input volumes 
+    Compute the Pearson correlation coefficient between the input volumes
     after rotating or displacing the first volume by the given quaternions
     or translations.
-    
+
     Parameters
     ----------
     mrc1 : numpy.ndarray, shape (n,n,n)
@@ -131,7 +123,7 @@ def score_deformations(mrc1, mrc2, warp):
         volume to be held fixed
     warp : numpy.ndarray, shape (n_quat,4) or (n_trans,3)
         rotations or displacements to apply to mrc2
-        
+
     Returns
     -------
     ccs : numpy.ndarray, shape (n_quat)
@@ -139,25 +131,33 @@ def score_deformations(mrc1, mrc2, warp):
     """
     # deform one of the input volumes by rotation or displacement
     if warp.shape[-1] == 4:
-        wmrc1 = rotate_volume(mrc1, warp)
+        wmrc1 = rotate_volume(mrc1, warp, xp, ndimage)
     elif warp.shape[-1] == 3:
         print("Not yet implemented")
         return
     else:
         print("Warp input must be quaternions or translations")
         return
-        
+
     # score each deformed volume using the Pearson CC
-    wmrc1_flat = wmrc1.reshape(wmrc1.shape[0],-1)
+    wmrc1_flat = wmrc1.reshape(wmrc1.shape[0], -1)
     mrc2_flat = xp.expand_dims(mrc2.flatten(), axis=0)
-    ccs = pearson_cc(wmrc1_flat, mrc2_flat)
+    ccs = pearson_cc(wmrc1_flat, mrc2_flat, xp)
     return ccs
 
-def scan_orientations_fine(mrc1, mrc2, opt_q, prev_score, n_iterations=10, n_search=420):
+def scan_orientations_fine(
+        mrc1,
+        mrc2,
+        opt_q,
+        prev_score,
+        xp,
+        ndimage,
+        n_iterations=10,
+        n_search=420):
     """
-    Perform a fine alignment search in the vicinity of the input quaternion 
-    to align mrc1 to mrc2. 
-    
+    Perform a fine alignment search in the vicinity of the input quaternion
+    to align mrc1 to mrc2.
+
     Parameters
     ----------
     mrc1 : numpy.ndarray, shape (n,n,n)
@@ -172,7 +172,7 @@ def scan_orientations_fine(mrc1, mrc2, opt_q, prev_score, n_iterations=10, n_sea
         number of iterations of alignment to perform
     n_search : int, default 420
         number of quaternions to score at each orientation
-        
+
     Returns
     -------
     opt_q : numpy.ndarray, shape (4)
@@ -181,28 +181,36 @@ def scan_orientations_fine(mrc1, mrc2, opt_q, prev_score, n_iterations=10, n_sea
         cross-correlation between aligned mrc1 and mrc2
     """
     # perform a series of fine alignment, ending if CC no longer improves
-    sigmas = 2-0.2*xp.arange(1,10)
+    sigmas = 2 - 0.2 * xp.arange(1, 10)
     for n in range(1, n_iterations):
-        quat = get_preferred_orientation_quat(n_search-1, float(sigmas[n-1]), base_quat=opt_q)
+        quat = get_preferred_orientation_quat(
+            n_search - 1, float(sigmas[n - 1]), xp, base_quat=opt_q)
         quat = xp.vstack((opt_q, quat))
-        ccs = score_deformations(mrc1, mrc2, quat)
+        ccs = score_deformations(mrc1, mrc2, quat, xp, ndimage)
         if xp.max(ccs) < prev_score:
             break
         else:
             opt_q = quat[xp.argmax(ccs)]
         #print(torch.max(ccs), opt_q) # useful for debugging
-        prev_score = xp.max(ccs)     
-    
+        prev_score = xp.max(ccs)
+
     return opt_q, prev_score
 
-def scan_orientations(mrc1, mrc2, n_iterations=10, n_search=420, nscs=1):
+def scan_orientations(
+        mrc1,
+        mrc2,
+        xp,
+        ndimage,
+        n_iterations=10,
+        n_search=420,
+        nscs=1):
     """
-    Find the quaternion and its associated score that best aligns volume mrc1 to mrc2. 
-    Candidate orientations are scored based on the Pearson correlation coefficient. 
-    First a coarse search is performed, followed by a series of increasingly fine 
-    searches in angular space. To prevent getting stuck in a bad solution, the top nscs 
+    Find the quaternion and its associated score that best aligns volume mrc1 to mrc2.
+    Candidate orientations are scored based on the Pearson correlation coefficient.
+    First a coarse search is performed, followed by a series of increasingly fine
+    searches in angular space. To prevent getting stuck in a bad solution, the top nscs
     solutions from the coarse grained search can be investigated.
-    
+
     Parameters
     ----------
     mrc1 : numpy.ndarray, shape (n,n,n)
@@ -215,7 +223,7 @@ def scan_orientations(mrc1, mrc2, n_iterations=10, n_search=420, nscs=1):
         number of quaternions to score at each orientation
     nscs : int, default 1
         number of solutions from the coarse-grained search to investigate
-        
+
     Returns
     -------
     opt_q : numpy.ndarray, shape (4)
@@ -225,25 +233,36 @@ def scan_orientations(mrc1, mrc2, n_iterations=10, n_search=420, nscs=1):
     """
     # perform a coarse alignment to start
     quat = xp.array(sk.get_uniform_quat(n_search)) # update for skopi
-    ccs = score_deformations(mrc1, mrc2, quat)
+    ccs = score_deformations(mrc1, mrc2, quat, xp, ndimage)
     ccs_order = xp.argsort(ccs)[::-1]
-    
+
     # scan the top solutions
-    opt_q_list, ccs_list = xp.zeros((nscs,4)), xp.zeros(nscs)
+    opt_q_list, ccs_list = xp.zeros((nscs, 4)), xp.zeros(nscs)
     for n in range(nscs):
         start_q, start_score = quat[ccs_order[n]], ccs[ccs_order[n]]
-        opt_q_list[n], ccs_list[n] = scan_orientations_fine(mrc1, mrc2, start_q, start_score, 
-                                                            n_iterations=n_iterations, n_search=n_search)
+        opt_q_list[n], ccs_list[n] = scan_orientations_fine(
+            mrc1, mrc2, start_q, start_score, xp, ndimage, n_iterations=n_iterations, n_search=n_search)
 
     opt_q, score = opt_q_list[xp.argmax(ccs_list)], xp.max(ccs_list)
     return opt_q, score
 
-def align_volumes(mrc1, mrc2, zoom=1, sigma=0, n_iterations=10, n_search=420, nscs=1, output=None, voxel_size=None):
+def align_volumes(
+        mrc1,
+        mrc2,
+        xp,
+        ndimage,
+        zoom=1,
+        sigma=0,
+        n_iterations=10,
+        n_search=420,
+        nscs=1,
+        output=None,
+        voxel_size=None):
     """
     Find the quaternion that best aligns volume mrc1 to mrc2. Volumes are
     optionally preprocessed by up / downsampling and applying a Gaussian
     filter.
-    
+
     Parameters
     ----------
     mrc1 : numpy.ndarray, shape (n,n,n)
@@ -264,57 +283,67 @@ def align_volumes(mrc1, mrc2, zoom=1, sigma=0, n_iterations=10, n_search=420, ns
         if supplied, save the aligned volume to this path in MRC format
     voxel_size : float, default None
         if supplied, use as value of voxel size in Angstrom for output
-    
+
     Returns
     -------
     r_vol : numpy.ndarray, shape (n,n,n)
-        copy of centered mrc1 aligned with centered mrc2 
+        copy of centered mrc1 aligned with centered mrc2
     mrc2_original : umpy.ndarray, shape (n,n,n)
         copy of centered mrc2
-    """    
+    """
     # center input volumes, then make copies
     mrc1, mrc2 = xp.array(mrc1), xp.array(mrc2)
-    mrc1, mrc2 = center_volume(mrc1), center_volume(mrc2)
+    mrc1, mrc2 = center_volume(
+        mrc1, xp, ndimage), center_volume(mrc2, xp, ndimage)
     mrc1_original = mrc1.copy()
     mrc2_original = mrc2.copy()
-    
+
     # optionally up/downsample volumes
     if zoom != 1:
-        mrc1 = ndimage.zoom(mrc1, (zoom,zoom,zoom))
-        mrc2 = ndimage.zoom(mrc2, (zoom,zoom,zoom))
-    
+        mrc1 = ndimage.zoom(mrc1, (zoom, zoom, zoom))
+        mrc2 = ndimage.zoom(mrc2, (zoom, zoom, zoom))
+
     # optionally apply a Gaussian filter to volumes
     if sigma != 0:
         mrc1 = ndimage.gaussian_filter(mrc1, sigma=sigma)
         mrc2 = ndimage.gaussian_filter(mrc2, sigma=sigma)
-    
+
     # evaluate both hands
-    opt_q1, cc1 = scan_orientations(mrc1, mrc2, n_iterations, n_search, nscs=nscs)
-    opt_q2, cc2 = scan_orientations(flip(mrc1, [0,1,2]), mrc2, n_iterations, n_search, nscs=nscs)
-    if cc1 > cc2: 
+    opt_q1, cc1 = scan_orientations(
+        mrc1, mrc2, xp, ndimage, n_iterations, n_search, nscs=nscs)
+    opt_q2, cc2 = scan_orientations(
+        flip(mrc1, [0, 1, 2], xp), mrc2, xp, ndimage, n_iterations, n_search, nscs=nscs)
+    if cc1 > cc2:
         opt_q, cc_r, invert = opt_q1, cc1, False
-    else: 
+    else:
         opt_q, cc_r, invert = opt_q2, cc2, True
-    print(f"Alignment CC after rotation is: {cc_r:.3f}")    
-    
+    print(f"Alignment CC after rotation is: {cc_r:.3f}")
+
     # generate final aligned map
     if invert:
         print("Map had to be inverted")
-        mrc1_original = flip(mrc1_original, [0,1,2])
-    r_vol = rotate_volume(mrc1_original, xp.expand_dims(opt_q, axis=0))[0]
-    final_cc = pearson_cc(xp.expand_dims(r_vol.flatten(), axis=0), xp.expand_dims(mrc2_original.flatten(), axis=0))
-    print(f"Final CC between unzoomed / unfiltered volumes is: {float(final_cc):.3f}")
-    if use_cupy:
+        mrc1_original = flip(mrc1_original, [0, 1, 2], xp)
+    r_vol = rotate_volume(mrc1_original, xp.expand_dims(
+        opt_q, axis=0), xp, ndimage)[0]
+    final_cc = pearson_cc(
+        xp.expand_dims(
+            r_vol.flatten(), axis=0), xp.expand_dims(
+             mrc2_original.flatten(), axis=0), xp)
+    print(
+        f"Final CC between unzoomed / unfiltered volumes is: {float(final_cc):.3f}")
+
+    # Convert CuPy array to NumPy
+    if not isinstance(r_vol, np.ndarray):
         r_vol = r_vol.get()
         mrc2_original = mrc2_original.get()
     if output is not None:
         save_mrc(output, np.array(r_vol), voxel_size=voxel_size)
-            
+
     return r_vol, mrc2_original
 
 # Work around for bug in cupy flip and its normalize axis indicies
-def flip(arr, orien):
-    if use_cupy:
+def flip(arr, orien, xp):
+    if not isinstance(arr, np.ndarray):
         return xp.array(np.flip(arr.get(), orien))
     else:
         return np.flip(arr, orien)
--- spinifel/eval/fsc.py
+++ spinifel/eval/fsc.py
@@ -1,11 +1,14 @@
-import argparse, time, os
+import matplotlib.pyplot as plt
+import argparse
+import time
+import os
 import numpy as np
-import h5py, mrcfile
+import h5py
+import mrcfile
 import scipy.interpolate
 from align import save_mrc, align_volumes
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 
 """
 Estimate resolution based on the Fourier shell correlation (FSC) between 
@@ -16,47 +19,58 @@ def parse_input():
     """
     Parse command line input.
     """
-    parser = argparse.ArgumentParser(description="Simulate a simple SPI dataset.")
-    parser.add_argument('-m', '--mrc_file', help='mrc file of reconstructed map', required=True, type=str)
-    parser.add_argument('-d', '--dataset', help='h5 file of simulated data', required=True, type=str)
-    parser.add_argument('-p', '--pdb_file', help='pdb file for reference structure', required=True, type=str)
-    parser.add_argument('-o', '--output', help='output directory for aligned volumes', required=False, type=str)
+    parser = argparse.ArgumentParser(
+        description="Simulate a simple SPI dataset.")
+    parser.add_argument(
+        '-m', '--mrc_file', help='mrc file of reconstructed map', required=True, type=str)
+    parser.add_argument(
+        '-d', '--dataset', help='h5 file of simulated data', required=True, type=str)
+    parser.add_argument(
+        '-p', '--pdb_file', help='pdb file for reference structure', required=True, type=str)
+    parser.add_argument(
+        '-o', '--output', help='output directory for aligned volumes', required=False, type=str)
     # optional arguments to adjust alignment protocol
-    parser.add_argument('--zoom', help='Zoom factor during alignment', required=False, type=float, default=1)
-    parser.add_argument('--sigma', help='Sigma for Gaussian filtering during alignment', required=False, type=float, default=0)
-    parser.add_argument('--niter', help='Number of alignment iterations to run', required=False, type=int, default=10)
-    parser.add_argument('--nsearch', help='Number of quaternions to score per iteration', required=False, type=int, default=360)
+    parser.add_argument('--zoom', help='Zoom factor during alignment',
+                        required=False, type=float, default=1)
+    parser.add_argument('--sigma', help='Sigma for Gaussian filtering during alignment',
+                        required=False, type=float, default=0)
+    parser.add_argument('--niter', help='Number of alignment iterations to run',
+                        required=False, type=int, default=10)
+    parser.add_argument('--nsearch', help='Number of quaternions to score per iteration',
+                        required=False, type=int, default=360)
+    parser.add_argument('--use-cupy', help='Use CuPy for GPU accelerated FSC calculation',
+                        required=False, type=bool, default=True)
 
     return vars(parser.parse_args())
 
-def get_reciprocal_mesh(voxel_number_1d, distance_reciprocal_max):
+def get_reciprocal_mesh(voxel_number_1d, distance_reciprocal_max, xp):
     """
     Get a centered, symetric mesh of given dimensions. Altered from skopi.
-    
+
     Parameters
     ----------
     voxel_number_1d : int
         number of voxels per axis
     distance_reciprocal_max : float
         maximum voxel resolution in inverse Angstrom
-    
+
     Returns
     -------
     reciprocal_mesh : numpy.ndarray, shape (n,n,n,3)
         grid of reciprocal space vectors for each voxel
     """
     max_value = distance_reciprocal_max
-    linspace = np.linspace(-max_value, max_value, voxel_number_1d)
-    reciprocal_mesh_stack = np.asarray(
-        np.meshgrid(linspace, linspace, linspace, indexing='ij'))
-    reciprocal_mesh = np.moveaxis(reciprocal_mesh_stack, 0, -1)
+    linspace = xp.linspace(-max_value, max_value, voxel_number_1d)
+    reciprocal_mesh_stack = xp.asarray(
+        xp.meshgrid(linspace, linspace, linspace, indexing='ij'))
+    reciprocal_mesh = xp.moveaxis(reciprocal_mesh_stack, 0, -1)
 
     return reciprocal_mesh
-    
-def compute_reference(pdb_file, M, distance_reciprocal_max):
+
+def compute_reference(pdb_file, M, distance_reciprocal_max, xp):
     """
     Compute the reference density map from a PDB file using skopi.
-    
+
     Parameters
     ----------
     pdb_file : string
@@ -73,24 +87,25 @@ def compute_reference(pdb_file, M, distance_reciprocal_max):
     """
     import skopi.gpu as pg
     import skopi as sk
-    
+
     # set up Particle object
     particle = sk.Particle()
     particle.read_pdb(pdb_file, ff='WK')
 
     # compute ideal diffraction volume and take FT for density map
-    mesh = get_reciprocal_mesh(M, distance_reciprocal_max)
-    cfield = pg.calculate_diffraction_pattern_gpu(mesh, particle, return_type='complex_field')
+    mesh = get_reciprocal_mesh(M, distance_reciprocal_max, np)
+    cfield = pg.calculate_diffraction_pattern_gpu(
+        mesh, particle, return_type='complex_field')
     ivol = np.square(np.abs(cfield))
     density = np.fft.fftshift(np.fft.ifftn(np.fft.ifftshift(cfield))).real
 
     return density
 
-def compute_fsc(volume1, volume2, distance_reciprocal_max, spacing=0.01, output=None):
+def compute_fsc(volume1, volume2, distance_reciprocal_max, xp, spacing=0.01, output=None):
     """
     Compute the Fourier shell correlation (FSC) curve, with the 
     estimated resolution based on a threshold of 0.5.
-    
+
     Parameters
     ----------
     volume1 : numpy.ndarray, shape (n,n,n)
@@ -109,30 +124,37 @@ def compute_fsc(volume1, volume2, distance_reciprocal_max, spacing=0.01, output=
     resolution : float
         estimated resolution of reconstructed map in Angstroms
     """
-    
-    mesh = get_reciprocal_mesh(volume1.shape[0], distance_reciprocal_max)
-    smags = np.linalg.norm(mesh, axis=-1).flatten() * 1e-10
-    r_spacings = np.arange(0, smags.max() / np.sqrt(3), spacing)
-    
-    ft1 = np.fft.fftshift(np.fft.fftn(volume1)).flatten()
-    ft2 = np.conjugate(np.fft.fftshift(np.fft.fftn(volume2)).flatten())
-    rshell, fsc = np.zeros(len(r_spacings)), np.zeros(len(r_spacings))
-    
-    for i,r in enumerate(r_spacings):
-        indices = np.where((smags>r) & (smags<r+spacing))[0]
-        numerator = np.sum(ft1[indices] * ft2[indices])
-        denominator = np.sqrt(np.sum(np.square(np.abs(ft1[indices]))) * np.sum(np.square(np.abs(ft2[indices]))))
-        rshell[i] = r + 0.5*spacing 
+
+    mesh = get_reciprocal_mesh(volume1.shape[0], distance_reciprocal_max, xp)
+    smags = xp.linalg.norm(xp.array(mesh), axis=-1).reshape(-1) * 1e-10
+    volume1 = xp.array(volume1)
+    volume2 = xp.array(volume2)
+    r_spacings = xp.arange(0, smags.max() / np.sqrt(3), spacing)
+
+    ft1 = xp.fft.fftshift(xp.fft.fftn(volume1)).reshape(-1)
+    ft2 = xp.conjugate(xp.fft.fftshift(xp.fft.fftn(volume2)).reshape(-1))
+    rshell, fsc = xp.zeros(len(r_spacings)), xp.zeros(len(r_spacings))
+
+    for i, r in enumerate(r_spacings):
+        indices = xp.where((smags > r) & (smags < r + spacing))[0]
+        numerator = xp.sum(ft1[indices] * ft2[indices])
+        denominator = xp.sqrt(xp.sum(
+            xp.square(xp.abs(ft1[indices]))) * xp.sum(xp.square(xp.abs(ft2[indices]))))
+        rshell[i] = r + 0.5 * spacing
         fsc[i] = numerator.real / denominator
-        
+
+    if not isinstance(fsc, np.ndarray):
+        fsc = fsc.get()
+        rshell = rshell.get()
+
     f = scipy.interpolate.interp1d(fsc, rshell)
     try:
-        resolution = 1.0/f(0.5)
+        resolution = 1.0 / f(0.5)
         print(f"Estimated resolution from FSC: {resolution:.1f} Angstrom")
     except ValueError:
         resolution = -1
         print("Resolution could not be estimated.")
-        
+
     # optionally plot
     if output is not None:
         f, ax1 = plt.subplots(figsize=(5,3))
@@ -143,7 +165,7 @@ def compute_fsc(volume1, volume2, distance_reciprocal_max, spacing=0.01, output=
         ax1.set_xlabel("Resolution (1/${\mathrm{\AA}}$)")
         ax1.set_ylabel("FSC", fontsize=12)
         f.savefig(os.path.join(output, "fsc.png"), dpi=300, bbox_inches='tight')
-        
+
     return resolution
 
 def main():
@@ -151,22 +173,31 @@ def main():
     args = parse_input()
     if not os.path.isdir(args['output']):
         os.mkdir(args['output'])
-    
+    if args['use_cupy']:
+        import cupy as xp
+        from cupyx.scipy import ndimage
+    else:
+        xp = np
+        from scipy import ndimage
+
     # load and prepare input files
     volume = mrcfile.open(args['mrc_file']).data.copy()
     with h5py.File(args['dataset'], "r") as f:
-        dist_recip_max = np.linalg.norm(f['pixel_position_reciprocal'][:], axis=-1).max()
-    reference = compute_reference(args['pdb_file'], volume.shape[0], dist_recip_max)
+        dist_recip_max = np.linalg.norm(
+            f['pixel_position_reciprocal'][:], axis=-1).max()
+    reference = compute_reference(
+        args['pdb_file'], volume.shape[0], dist_recip_max, xp)
 
     # align volumes
-    ali_volume, ali_reference = align_volumes(volume, reference, zoom=args['zoom'], sigma=args['sigma'], 
+    ali_volume, ali_reference = align_volumes(volume, reference, xp, ndimage, zoom=args['zoom'], sigma=args['sigma'],
                                               n_iterations=args['niter'], n_search=args['nsearch'])
     if args['output'] is not None:
         save_mrc(os.path.join(args['output'], "reference.mrc"), ali_reference)
         save_mrc(os.path.join(args['output'], "aligned.mrc"), ali_volume)
 
     # compute fsc
-    resolution = compute_fsc(ali_reference, ali_volume, dist_recip_max, output=args['output'])
+    resolution = compute_fsc(ali_reference, ali_volume,
+                             dist_recip_max, xp, output=args['output'])
 
 if __name__ == '__main__':
     main()
