5f9fd179cf9debc7593354ff364f2b17698facb9
Seema Mirchandaney <seemah@stanford.edu>
2022-07-27 12:41:10 -0700

    add streaming psana/legion main entry point

--- legion_main.py
+++ legion_main.py
@@ -6,6 +6,8 @@ from spinifel.legion import main
 if __name__ == '__main__':
     if settings.verbose:
         print(settings)
-
-
-    main()
+    if settings.mode == "legion_psana2":
+        from spinifel.legion import main_psana2
+        main_psana2()
+    else:
+        main()
