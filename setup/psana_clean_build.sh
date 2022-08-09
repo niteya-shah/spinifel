0972dce900a86c8abbc09ae3498f19b345b8d546
monarin <monarin@gmail.com>
2022-08-09 01:33:48 -0400

    create easy-install.pth manually for setuptools 46.4.0

--- setup/psana_clean_build.sh
+++ setup/psana_clean_build.sh
@@ -10,3 +10,14 @@ git clean -fxd
 popd
 
 "$root_dir"/psana_dirty_build.sh
+
+# FIXME: With setuptools pinned to 46.4.0, the current psana2 python packages
+# will be installed into site-packages withou easy-install.pth file. This file
+# points to the source folders where the modules are for 'development' mode.
+# This is a hack to create the easy-install.pth file for psana2 and to be
+# removed if we upgrade setuptools. Note!!! that newer setuptools breaks legion
+# - see issue#48 for more detail.
+cat << EOF > $LCLS2_DIR/install/lib/python$PYVER/site-packages/easy-install.pth
+$LCLS2_DIR/psalg
+$LCLS2_DIR/psana
+EOF
