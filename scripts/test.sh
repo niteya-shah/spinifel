eac9e925aabdc909dacfe155fac18f049847e539
monarin <monarin@gmail.com>
2022-09-05 22:08:01 -0400

    wrap tests that require mpi with jsrun/srun

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,7 +11,22 @@ export PYTHONPATH="$PYTHONPATH:$root_dir"
 
 set -x
 
-# Pickup all tests spinifel modules
-#pytest -s tests           
-pytest -s tests/test_FSC.py
+# Pickup all tests spinifel modules (anything that don't import mpi)
+#pytest -s tests
+#pytest -s spinifel/tests
+
+# Running pytest on scripts that potentially use mpi fails (originally 
+# seen in psana2 branch - see issue#56) The solution right now is to wrap 
+# it under jsrun/srun.
+target=${SPINIFEL_TARGET:-${NERSC_HOST:-$(hostname --fqdn)}}
+echo "[spinifel/tests/withmpi] target: $target"
+
+if [[ ${target} = "cgpu"* ]]; then
+    SPINIFEL_TEST_LAUNCHER="srun"
+elif [[ ${target} = *"summit"* || ${target} = *"ascent"* ]]; then
+    SPINIFEL_TEST_LAUNCHER="jsrun"
+fi
+    
+$SPINIFEL_TEST_LAUNCHER -n1 -g1 pytest -s tests/withmpi
+$SPINIFEL_TEST_LAUNCHER -n1 -g1 pytest -s spinifel/tests/withmpi
 
