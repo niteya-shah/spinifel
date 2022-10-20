a7ac29209fbcafbcb66c4cb88dcf2e4280926631
Seema Mirchandaney <seemah@stanford.edu>
2022-10-20 15:40:54 -0700

    change xtc2 file

--- submit_perlmutter_final_demo.sh
+++ submit_perlmutter_final_demo.sh
@@ -24,7 +24,7 @@ for n in 1 2 4 8 ; do
     echo "Running SPINIFEL with " $ntasks " ranks"
     OUT_DIR_LEGION="${SCRATCH}/spinifel_output_legion/result_${ntasks}tasks"
     mkdir -p $OUT_DIR_LEGION
-    srun -N $n --ntasks=$ntasks --ntasks-per-node=4 -G $ntasks --gpus-per-task=1 --gpu-bind=single:1 legion_python -ll:py 1 -ll:pyomp 1  -ll:csize 32768 legion_main.py --default-settings=perlmutter_converge.toml --mode=legion_psana2 data.out_dir=$OUT_DIR_LEGION algorithm.N_images_max=10000 psana.enable=true psana.ps_dir="/global/cfs/cdirs/m2859/data/3iyf/demo/xtc2" algorithm.N_generations=20 runtime.chk_convergence=false runtime.cupy_mempool_clear=false algorithm.N_gens_stream=2 algorithm.N_batch_size=1000
+    srun -N $n --ntasks=$ntasks --ntasks-per-node=4 -G $ntasks --gpus-per-task=1 --gpu-bind=single:1 legion_python -ll:py 1 -ll:pyomp 1  -ll:csize 32768 legion_main.py --default-settings=perlmutter_converge.toml --mode=legion_psana2 data.out_dir=$OUT_DIR_LEGION algorithm.N_images_max=10000 psana.enable=true psana.ps_dir="/global/cfs/cdirs/m2859/data/3iyf/xtc2" algorithm.N_generations=20 runtime.chk_convergence=false runtime.cupy_mempool_clear=false algorithm.N_gens_stream=2 algorithm.N_batch_size=1000
 t_end=`date +%s`
 echo PSJobCompleted TotalElapsed $((t_end-t_start)) $t_start $t_end
 done
