f302a67fb3d16ec4cf9c73b36ecb0574f42d49c6
Monarin Uervirojnangkoorn <monarin@gmail.com>
2023-04-14 18:59:24 -0400

    check out manually from forked gitsubmodule

--- setup/rebuild_cufinufft.sh
+++ setup/rebuild_cufinufft.sh
@@ -10,6 +10,10 @@ target=${SPINIFEL_TARGET:-${NERSC_HOST:-$(hostname --fqdn)}}
 echo $target
 pushd "$root_dir"/cufinufft
 
+# ***TMP*** Checking out specific branch from a forked gitsubmodule
+git checkout djh/PyBindGPU
+
+
 if [[ ${target} = "cgpu"* ]]; then
     make -j${THREADS:-8} site=nersc_cgpu lib
 elif [[ ${target} = "perlmutter"* ]]; then
@@ -19,7 +23,7 @@ elif [[ ${target} = *"summit"* || ${target} = *"ascent"* ]]; then
 elif [[ ${target} = "psbuild"* ]]; then
     export NVCCFLAGS="-std=c++17 -ccbin=${CXX} -O3 ${NVARCH} -Wno-deprecated-gpu-targets --default-stream per-thread -Xcompiler "\"${CXXFLAGS}\"
     make -j${THREADS:-8} site=psbuild lib
-elif [[ ${target} = *"crusher"* ]]; then
+elif [[ ${target} = *"crusher"* || ${target} = *"frontier"* ]]; then
     make -j${THREADS:-8} site=olcf_crusher 
 elif [[ ${target} = "g0"*".stanford.edu" ]]; then # sapling
     export NVCCFLAGS="-std=c++17 -ccbin=${CXX} -O3 ${NVARCH} -Wno-deprecated-gpu-targets --compiler-options=-fPIC --default-stream per-thread -Xcompiler "\"${CXXFLAGS}\"
@@ -37,7 +41,7 @@ echo CUFINUFFT_DIR is $CUFINUFFT_DIR
 if [[ ${target} = "psbuild"* ]]; then
     conda install -y -c conda-forge pycuda=2022.1
     LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUFINUFFT_DIR:/opt/nvidia/usr/lib64 pip install --no-cache-dir .
-elif [[ ${target} = *"crusher"* ]]; then
+elif [[ ${target} = *"crusher"* || ${target} = *"frontier"* ]]; then
     LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUFINUFFT_DIR pip install --no-cache-dir .
 else
     pip install --no-cache-dir pycuda==2022.1
