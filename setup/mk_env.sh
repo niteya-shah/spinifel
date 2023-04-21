182c12a447c80d365bb3fcc469c7715920a04cfc
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-04-21 18:14:44 -0400

    Use ROCm 5.4.3 on Frontier.

--- setup/mk_env.sh
+++ setup/mk_env.sh
@@ -239,7 +239,7 @@ EOF
 elif [[ $(hostname --fqdn) = *".frontier."* ]]; then
     cat >> env.sh <<EOF
 module load PrgEnv-gnu
-module load rocm/5.1.0
+module load rocm/5.4.3
 #module use /ccs/proj/chm137/shared/stf006/djh992/llvm_bjoo/modules # Copied from crusher
 #module load amd-llvm    # loading this changed rocm to 5.30 on frontier
 module load cray-fftw
@@ -254,7 +254,6 @@ export MPI4PY_MPICC="\$(which cc) --shared"
 
 # Make sure Cray-FFTW get loaded first to avoid Conda's MKL
 export LD_PRELOAD="\${FFTW_DIR}/libfftw3.so"
-export LD_LIBRARY_PATH=/opt/rocm-5.1.0/hipfft/lib:$LD_LIBRARY_PATH
 
 export LEGION_USE_GASNET=${LEGION_USE_GASNET:-1}
 export GASNET_CONDUIT=${GASNET_CONDUIT:-ofi-slingshot11}
