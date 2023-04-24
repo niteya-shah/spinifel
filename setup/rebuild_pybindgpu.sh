d50f70f1c8bece5fc7b923c64d719db46abc73ca
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-04-24 17:55:36 -0400

    Update installation code for PybindGPU.

--- setup/rebuild_pybindgpu.sh
+++ setup/rebuild_pybindgpu.sh
@@ -9,20 +9,11 @@ source "$root_dir"/env.sh
 target=${SPINIFEL_TARGET:-${NERSC_HOST:-$(hostname --fqdn)}}
 echo $target
 
-pushd "$root_dir"/PybindGPU/PyGPU
-
+pushd "$root_dir"/PybindGPU
 if [[ ${target} = *"crusher"* || ${target} = *"frontier"* ]]; then
-    make
-elif [[ ${target} = *"spock"* ]]; then
-    echo "Currently unsupported"
-    exit
+    PYBIND_GPU_TARGET=gfx90a pip install --no-cache-dir -e .
 else
     echo "We do not need PybindGPU for this architecture"
     exit
-fi
-popd
-pushd "$root_dir"/PybindGPU
-if [[ ${target} = *"crusher"* || ${target} = *"frontier"* ]]; then
-    pip install --no-cache-dir -e .
 fi
 popd
