fdb2f19c2d7bebc3245a838258a019c34b853b5d
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-08-17 09:56:01 -0700

    Cleanup Legion configuration.

--- setup/reconfigure_legion.sh
+++ setup/reconfigure_legion.sh
@@ -21,24 +21,30 @@ else
     export LDFLAGS="-Wl,-rpath,$CONDA_ENV_DIR/lib"
 fi
 
-cmake -DCMAKE_PREFIX_PATH="$CONDA_ENV_DIR" \
-    -DCMAKE_BUILD_TYPE=$([ $LEGION_DEBUG -eq 1 ] && echo Debug || echo Release) \
-    -DBUILD_SHARED_LIBS=ON \
-    -DLegion_BUILD_BINDINGS=ON \
-    -DLegion_ENABLE_TLS=ON \
-    -DLegion_USE_Python=ON \
-    -DPYTHON_EXECUTABLE="$(which python)" \
-    -DLegion_USE_CUDA=OFF \
-    -DLegion_USE_OpenMP=ON \
-    -DLegion_USE_GASNet=$([ $LEGION_USE_GASNET -eq 1 ] && echo ON || echo OFF) \
-    -DGASNet_ROOT_DIR="$GASNET_ROOT" \
-    -DGASNet_CONDUITS=${LEGION_GASNET_CONDUIT:-$GASNET_CONDUIT} \
-    -DLegion_NETWORKS=gasnetex \
-    -DLegion_MAX_NUM_NODES=8192 \
-    -DLegion_USE_HDF5=ON \
-    -DLegion_MAX_DIM=4 \
-    -DCMAKE_INSTALL_PREFIX="$LEGION_INSTALL_DIR" \
-    -DCMAKE_INSTALL_LIBDIR="$LEGION_INSTALL_DIR/lib" \
-    "$root_dir"/legion
+legion_cmake_flags=(
+    -DCMAKE_PREFIX_PATH="$CONDA_ENV_DIR"
+    -DCMAKE_BUILD_TYPE=$([ $LEGION_DEBUG -eq 1 ] && echo Debug || echo Release)
+    -DBUILD_SHARED_LIBS=ON
+    -DLegion_BUILD_BINDINGS=ON
+    -DLegion_USE_Python=ON
+    -DPYTHON_EXECUTABLE="$(which python)"
+    -DLegion_USE_CUDA=OFF
+    -DLegion_USE_OpenMP=ON
+    -DLegion_USE_HDF5=ON
+    -DLegion_MAX_DIM=4
+    -DCMAKE_INSTALL_PREFIX="$LEGION_INSTALL_DIR"
+    -DCMAKE_INSTALL_LIBDIR="$LEGION_INSTALL_DIR/lib"
+)
+
+if [[ $LEGION_USE_GASNET -eq 1 ]]; then
+    legion_cmake_flags+=(
+        -DLegion_NETWORKS=gasnetex
+        -DGASNet_ROOT_DIR="$GASNET_ROOT"
+        -DGASNet_CONDUITS=${LEGION_GASNET_CONDUIT:-$GASNET_CONDUIT}
+        -DLegion_MAX_NUM_NODES=8192
+    )
+fi
+
+cmake "${legion_cmake_flags[@]}" "$root_dir"/legion
 
 popd
