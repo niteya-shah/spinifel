f7c060d4aa04784f1e870c50aefcecd062245d60
niteya-shah <niteya.56@gmail.com>
2023-09-19 13:15:56 -0400

    fixes

--- spinifel/mpi/orientation_matching.py
+++ spinifel/mpi/orientation_matching.py
@@ -1,9 +1,5 @@
 import PyNVTX as nvtx
 
-from spinifel.sequential.orientation_matching import (
-    slicing_and_match as sequential_match,
-)
-
 from spinifel.sequential.orientation_matching import SNM
 from spinifel.extern.orientations_ext import halo_generator
 from spinifel import settings, contexts, Profiler
@@ -171,23 +167,3 @@ class SNM_MPI(SNM):
     def slicing_and_match(self, ac):
         orients, dist = self.slicing_and_match_with_min_dist(ac)
         return orients
-
-@nvtx.annotate("mpi/orientation_matching.py", is_prefix=True)
-def match(
-    ac,
-    slices_,
-    pixel_position_reciprocal,
-    pixel_distance_reciprocal,
-    ref_orientations=None,
-):
-    # The reference orientations don't have to match exactly between ranks.
-    # Each rank aligns its own slices.
-    # We can call the sequential function on each rank, provided that the
-    # cost of generating the model_slices isn't prohibitive.
-    return sequential_match(
-        ac,
-        slices_,
-        pixel_position_reciprocal,
-        pixel_distance_reciprocal,
-        ref_orientations=ref_orientations,
-    )
